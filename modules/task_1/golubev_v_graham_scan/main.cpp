890cca0632677e554ef28622c19894a7085ad223
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-02-27 23:25:27 +0300

    add folders

---
+++ modules/task_1/golubev_v_graham_scan/main.cpp
@@ -0,0 +1,21 @@
+// Copyright 2018 Nesterov Alexander
+#include <gtest/gtest.h>
+#include <vector>
+#include "./golubev_v_graham_scan.h"
+
+TEST(Sequential_graham_scan, Test_1) {
+  ASSERT_EQ(1, 1);
+}
+
+TEST(Sequential_graham_scan, Test_2) {
+  ASSERT_EQ(1, 1);
+}
+
+TEST(Sequential_graham_scan, Test_3) {
+  ASSERT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
