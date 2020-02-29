34f774720637265b4f2efac01a7fad62067b23a7
Yury Gaydaychuk <agri1987cola@gmail.com>
2020-03-01 00:15:24 +0300

    lint_fix

--- modules/task_1/gaydaychuk_bitwise_oddeven_sort/bitwise_oddeven_sort.h
+++ modules/task_1/gaydaychuk_bitwise_oddeven_sort/bitwise_oddeven_sort.h
@@ -9,4 +9,4 @@ int getMax(std::vector<int> *vec);
 void sortByDigit(std::vector<int> *vec, int exp);
 void bitwiseSort(std::vector<int> *vec);
 
-#endif // MODULES_TASK_1_GAYDAYCHUK_BITWISE_ODDEVEN_SORT_BITWISE_ODDEVEN_SORT_H_
+#endif //  MODULES_TASK_1_GAYDAYCHUK_BITWISE_ODDEVEN_SORT_BITWISE_ODDEVEN_SORT_H_
