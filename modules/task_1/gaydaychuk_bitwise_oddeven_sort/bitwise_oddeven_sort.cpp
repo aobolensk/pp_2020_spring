072812c93713109212784c387164451538ca7afe
Yury Gaydaychuk <agri1987cola@gmail.com>
2020-02-29 22:42:56 +0300

    lint_fixed

--- modules/task_1/gaydaychuk_bitwise_oddeven_sort/bitwise_oddeven_sort.cpp
+++ modules/task_1/gaydaychuk_bitwise_oddeven_sort/bitwise_oddeven_sort.cpp
@@ -1,7 +1,6 @@
 // Copyright 2020 Gaydaychuk Yury
 #include <vector>
 #include <string>
-#include <random>
 #include <ctime>
 #include "../../../modules/task_1/gaydaychuk_bitwise_oddeven_sort/bitwise_oddeven_sort.h"
 
@@ -12,45 +11,44 @@ bool checkAscending(std::vector<int> vec) {
     }
     for (int i = 1; i < n; i++) {
         if (vec[i - 1] > vec[i]) {
-            return false; 
+            return false;
         }
     }
-    return true; 
+    return true;
 }
 
 int getMax(std::vector<int> vec) {
     int n = vec.size();
-    int max = vec[0]; 
+    int max = vec[0];
     for (int i = 1; i < n; i++) {
         if (vec[i] > max) {
-            max = vec[i]; 
+            max = vec[i];
         }
     }
-    return max; 
+    return max;
 }
 
-void sortByDigit(std::vector<int> &vec, int digit) { 
+void sortByDigit(std::vector<int> &vec, int digit) {
     int n = vec.size();
     std::vector<int> res(n);
-    int i, count[10] = {0}; 
-  
+    int i, count[10] = {0};
     for (i = 0; i < n; i++) {
-        count[(vec[i] / digit) % 10]++; 
+        count[(vec[i] / digit) % 10]++;
     }
     for (i = 1; i < 10; i++) {
-        count[i] += count[i - 1]; 
+        count[i] += count[i - 1];
     }
-    for (i = n - 1; i >= 0; i--) { 
-        res[count[(vec[i] / digit) % 10] - 1] = vec[i]; 
-        count[(vec[i] / digit) % 10]--; 
+    for (i = n - 1; i >= 0; i--) {
+        res[count[(vec[i] / digit) % 10] - 1] = vec[i];
+        count[(vec[i] / digit) % 10]--;
     }
     for (i = 0; i < n; i++) {
-        vec[i] = res[i]; 
+        vec[i] = res[i];
     }
 }
 
 void bitwiseSort(std::vector<int> &vec) {
     int max = getMax(vec);
-    for (int digit = 1; max / digit > 0; digit *= 10) 
-        sortByDigit(vec, digit); 
+    for (int digit = 1; max / digit > 0; digit *= 10)
+        sortByDigit(vec, digit);
 }
