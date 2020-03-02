3c01f877cdea103c4a6e4482197f74e6246b3d34
Arseniy Obolenskiy <gooddoog@student.su>
2020-03-02 20:28:57 +0300

    Implement OpenMP version of Gaussian image filtering and add tests

---
+++ modules/task_2/obolenskiy_a_gaussian_image_filtering/gaussian_image_filtering.h
@@ -0,0 +1,20 @@
+// Copyright 2020 Obolenskiy Arseniy
+#ifndef MODULES_TASK_2_OBOLENSKIY_A_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
+#define MODULES_TASK_2_OBOLENSKIY_A_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
+#include <cstdint>
+#include <vector>
+
+struct Pixel {
+    uint8_t r, g, b;
+    Pixel() = default;
+    ~Pixel() = default;
+};
+
+bool operator==(const Pixel &a, const Pixel &b);
+
+using Image = std::vector <Pixel>;
+
+Image generateRandomImage(int rows, int cols);
+Image gaussianFilter(const Image &a, int rows, int cols);
+
+#endif  // MODULES_TASK_2_OBOLENSKIY_A_GAUSSIAN_IMAGE_FILTERING_GAUSSIAN_IMAGE_FILTERING_H_
