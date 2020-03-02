a4641d29ab4952858dcc837f57c876dd370f2091
ivanvikhrev <l4ind@mail.ru>
2020-03-02 11:12:07 +0300

    fix-style

--- modules/task_1/vikhrev_sobel_edge_detection/sobel_edge_detection.h
+++ modules/task_1/vikhrev_sobel_edge_detection/sobel_edge_detection.h
@@ -4,71 +4,50 @@
 #define MODULES_TASK_1_VIKHREV_SOBEL_EDGE_DETECTION_SOBEL_EDGE_DETECTION_H_
 
 #include <vector>
-#include <string>
 #include <random>
 #include <iostream>
 
-
 template <class T>
 struct image {
-	    std::vector<T> data;
-		int rows{ 0 };
-		int cols{ 0 };
-		int size{ 0 };
-		image() {};
-		image(int r, int c);
-		image(int r, int c, std::vector<T> d);
-		image(const image<T>& img);
-		void print() {
-			for (auto a : data) {
-				std::cout << static_cast<int>(a) << " ";
-			}
-		}
-		//bool operator==(const  image<T>& img1, const image<T>& img2) {
-		//	if (img1.rows == img2.rows and i)
-		//}
+        std::vector<T> data;
+        int rows{ 0 };
+        int cols{ 0 };
+        int size{ 0 };
+        image() {}
+        image(int r, int c);
+        image(int r, int c, std::vector<T> d);
+        image(const image<T>& img);
 };
 
 /*----------------------------------------------------------------------*/
 template<class T>
 image<T>::image(int r, int c) {
-	size = r * c;
-	rows = r;
-	cols = c;
-	data.assign(size, 0);
-	//std::random_device rnd_device;
-	//std::mt19937 mersenne_engine{ rnd_device() };
-	//std::uniform_int_distribution<int> uint_dist{ 0,256 };
-	//auto gen = [&uint_dist, &mersenne_engine]() {
-	//	return uint_dist(mersenne_engine);
-	//};
-
-	//generate(begin(data), end(data), gen);
-	//for (auto a : data) {
-	//	std::cout << static_cast<int>(a) << " ";
-	//}
+    size = r * c;
+    rows = r;
+    cols = c;
+    data.assign(size, 0);
 }
 
 template<class T>
 image<T>::image(int r, int c, std::vector<T> d) {
-	size = r * c;
-	rows = r;
-	cols = c;
-	data = d;
+    size = r * c;
+    rows = r;
+    cols = c;
+    data = d;
 }
 
 template<class T>
 image<T>::image(const image<T> &img) {
-	size = img.size;
-	rows = img.rows;
-	cols = img.cols;
-	data = img.data;
+    size = img.size;
+    rows = img.rows;
+    cols = img.cols;
+    data = img.data;
 }
 
-using imageU=image<uint8_t>;
-using imageS=image<char>;
+using imageU = image<uint8_t>;
+using imageS = image<char>;
 
-imageU  sobel(imageU& img);
+imageU  sobel(const imageU& img);
 
 
 #endif  // MODULES_TASK_1_VIKHREV_SOBEL_EDGE_DETECTION_SOBEL_EDGE_DETECTION_H_
