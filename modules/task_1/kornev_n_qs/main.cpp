e5f54acdfbdc9a001c1e7a5042df1492dd51ab62
KornevNikita <>
2020-03-02 23:38:17 +0300

    add sequential qs and tests

---
+++ modules/task_1/kornev_n_qs/main.cpp
@@ -0,0 +1,106 @@
+// Copyright 2020 Kornev Nikita
+#include <utility>
+#include <algorithm>
+#include "gtest/gtest.h"
+#include "../../../modules/task_1/kornev_n_qs/qs.h"
+
+TEST(seq_qs, corr_res) {
+  int n = 100000;
+  double* a = new double[n], *b = new double[n];
+  get_rand_arr(a, n);
+
+  for (int i = 0; i < n; i++) {
+    b[i] = a[i];
+  }
+
+  std::sort(&b[0], &b[n]);
+  qs(a, 0, n - 1);
+
+  bool flag = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] != b[i]) {
+      flag = 1;
+      break;
+    }
+  }
+
+  ASSERT_EQ(flag, 0);
+}
+
+TEST(seq_qs, sorted_arr) {
+  int n = 100000;
+  double* a = new double[n], *b = new double[n];
+  get_rand_arr(a, n);
+
+  std::sort(&a[0], &a[n]);
+
+  for (int i = 0; i < n; i++) {
+    b[i] = a[i];
+  }
+  qs(a, 0, n - 1);
+
+  bool flag = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] != b[i]) {
+      flag = 1;
+      break;
+    }
+  }
+
+  ASSERT_EQ(flag, 0);
+}
+
+TEST(seq_qs, inverted_arr) {
+  int n = 100000;
+  double* a = new double[n], *b = new double[n];
+  get_rand_arr(a, n);
+
+  std::sort(&a[0], &a[n]);
+  for (int i = 0; i < n; i++) {
+    b[i] = a[i];
+  }
+
+  for (int i = 0; i < n / 2; i++) {
+    std::swap(a[i], a[n - i - 1]);
+  }
+
+  qs(a, 0, n - 1);
+
+  bool flag = 0;
+  for (int i = 0; i < n; i++) {
+    if (a[i] != b[i]) {
+      flag = 1;
+      break;
+    }
+  }
+
+  ASSERT_EQ(flag, 0);
+}
+
+TEST(seq_qs, almost_empty_arr) {
+  int n = 100000;
+  double* a = new double[n];
+  a[n - 1] = 1;
+
+  qs(a, 0, n - 1);
+
+  ASSERT_EQ(a[n - 1], 1);
+}
+
+TEST(seq_qs, empty_arr) {
+  int n = 100000;
+  double* a = new double[n];
+
+  for (int i = 0; i < n; i++) {
+    a[i] = 0;
+  }
+
+  qs(a, 0, n - 1);
+
+  double sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += a[i];
+  }
+
+  ASSERT_EQ(sum, 0);
+}
