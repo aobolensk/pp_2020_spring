ddc9323eb667d9f1f87d58de8f0a0ffe00d8c1e7
3817061LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2020-03-03 21:19:33 +0300

    Лабораторная работа 1. Джарвис

---
+++ modules/task_1/lembrikov_s_jarvis/jarvis.h
@@ -0,0 +1,13 @@
+// Copyright 2020 Lembrikov Stepan
+#ifndef MODULES_TASK_1_LEMBRIKOV_S_JARVIS_JARVIS_H_
+#define MODULES_TASK_1_LEMBRIKOV_S_JARVIS_JARVIS_H_
+
+#include <iostream>
+#include <random>
+#include <vector>
+
+std::vector<std::pair<double, double>> getRandomPoints(int n);
+std::vector<std::pair<double, double>> getNegativePoints(int n);
+std::vector<std::pair<double, double>> Jarvis(std::vector<std::pair<double, double>> points);
+
+#endif  // MODULES_TASK_1_LEMBRIKOV_S_JARVIS_JARVIS_H_
