9dd4c332350357d3381f5a8740a94edd6e0ce2d4
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-03-05 20:30:57 +0300

    fix

--- modules/task_2/golubev_v_graham_scan/main.cpp
+++ modules/task_2/golubev_v_graham_scan/main.cpp
@@ -15,9 +15,6 @@ TEST(DISABLED_Sequential_graham_scan, omp_sort) {
   int size = 100000000;
   std::vector<double> res_omp(size);
   std::vector<double> trash(size);
-  /*for (int i = 0; i < res_omp.size(); ++i) {
-    res_omp[i] = rand();
-  }*/
   std::vector<double> res_seq(res_omp);
 
   double t1 = omp_get_wtime();
@@ -38,7 +35,7 @@ TEST(Sequential_graham_scan, Test_merge) {
   int size = 11111114;
   std::mt19937 gen;
   std::vector<double> v(size);
-  for (int i = 0; i < v.size(); ++i) {
+  for (std::size_t i = 0; i < v.size(); ++i) {
     v[i] = gen() % 1000;
   }
   std::vector<double> check(v);
@@ -49,7 +46,7 @@ TEST(Sequential_graham_scan, Test_merge) {
   std::cout << "std_sort " << t2 - t1 << std::endl;
 
   t1 = omp_get_wtime();
-  mp_sort(v.begin(), v.end(), 3);
+  mp_sort(v.begin(), v.end(), 2);
   t2 = omp_get_wtime();
   std::cout << "omp_sort " << t2 - t1 << std::endl;
 
