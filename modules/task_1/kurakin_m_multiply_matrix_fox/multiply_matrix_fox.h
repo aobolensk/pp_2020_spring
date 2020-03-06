e7e4ebb4420d7763b34abacdabbe38a4830c8e02
MacLin123 <wallsoftheb77@gmail.com>
2020-03-06 17:41:13 +0300

    unsigned int typedef added

--- modules/task_1/kurakin_m_multiply_matrix_fox/multiply_matrix_fox.h
+++ modules/task_1/kurakin_m_multiply_matrix_fox/multiply_matrix_fox.h
@@ -4,6 +4,7 @@
 #include <vector>
 
 typedef std::vector<std::vector<double>> matrix;
+typedef unsigned int uint;
 bool canMultiplicate(const matrix &a, const matrix &b);
 void prepareOutMatrix(matrix* matrixOut, uint sizeRow, uint sizeCol);
 bool initMatrixRand(matrix* a, uint sizeRow, uint sizeCol);
