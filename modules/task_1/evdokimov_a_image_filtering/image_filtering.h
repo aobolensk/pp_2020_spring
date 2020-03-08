01295ab7d815f67a80fd7f36828dbac07e4c4b59
black8cherry <art8evd@gmail.com>
2020-03-08 11:11:35 +0300

    more spaces, moooore

--- modules/task_1/evdokimov_a_image_filtering/image_filtering.h
+++ modules/task_1/evdokimov_a_image_filtering/image_filtering.h
@@ -1,4 +1,4 @@
-//  Copyright 2019 Evdokimov Artem
+// Copyright 2019 Evdokimov Artem
 #ifndef MODULES_TASK_1_EVDOKIMOV_A_IMAGE_FILTERING_IMAGE_FILTERING_H_
 #define MODULES_TASK_1_EVDOKIMOV_A_IMAGE_FILTERING_IMAGE_FILTERING_H_
 
@@ -9,4 +9,4 @@ std::vector<int> generateImage(int, int);
 std::vector<int> getTempImage(std::vector<int>, int, int);
 std::vector<int> imageFiltering(std::vector<int>, std::vector<int>, int, int);
 
-#endif //  MODULES_TASK_1_EVDOKIMOV_A_IMAGE_FILTERING_IMAGE_FILTERING_H_
+#endif  // MODULES_TASK_1_EVDOKIMOV_A_IMAGE_FILTERING_IMAGE_FILTERING_H_
