6ba7b125ee639ae3cde2db371f2271c19a0ba01d
381706-1VlasovAndrey <andrio.vlasov.2000@mail.ru>
2020-03-09 04:09:05 +0300

    add all

--- modules/task_2/vlasov_a_multi_integration_simpson/multi_integration_simpson.h
+++ modules/task_2/vlasov_a_multi_integration_simpson/multi_integration_simpson.h
@@ -6,10 +6,13 @@
 #include <vector>
 #include <functional>
 
+double getIntegralSimpsonOpenMP(const std::function<double(const std::vector<double>&)>& f,
+  const std::vector<double>& a, const std::vector<double>& b, int n);
+
 double getSum(const std::function<double(const std::vector<double>&)>& f,
   const std::vector<double>& a, const std::vector<double>& b, int n);
 
-double getIntegralSimpsonOpenMP(const std::function<double(const std::vector<double>&)>& f,
+double getSum_odd(const std::function<double(const std::vector<double>&)>& f,
   const std::vector<double>& a, const std::vector<double>& b, int n);
 
 #endif  // MODULES_TASK_2_VLASOV_A_MULTI_INTEGRATION_SIMPSON_MULTI_INTEGRATION_SIMPSON_H_
