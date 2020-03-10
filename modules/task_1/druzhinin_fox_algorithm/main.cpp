5129f882f2f2590980519b2a7d3276a07bec998e
Enreyz <enreys@mail.ru>
2020-03-10 23:06:12 +0300

    idk

--- modules/task_1/druzhinin_fox_algorithm/main.cpp
+++ modules/task_1/druzhinin_fox_algorithm/main.cpp
@@ -70,6 +70,8 @@ TEST(Fox_Algorithm, Blocked_Matrix_Multiplication_Correct) {
 
     // Act
     blockMatrixMult(a, b, 3, res);
+	for (int i = 0; i < 9; i++)
+        std::cout << res[i] << " ";
     result = comparisonMatrixes(res, true_res, 3);
 
     // Assert
