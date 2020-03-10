0a7b789a985f55dad1128a9c510c78ca4b9b7a0c
381706-1Mityagina <darya.mityagina.1999@mail.com>
2020-03-10 15:55:38 +0300

    beautification of the code

--- modules/task_1/mityagina_d_increasing_the_contrast/increasing_the_contrast.h
+++ modules/task_1/mityagina_d_increasing_the_contrast/increasing_the_contrast.h
@@ -4,10 +4,10 @@
 
 #include <vector>
 
-std::vector <int> generate_im(int im_width, int im_height);
-std::vector <int> contrast_increase(std::vector <int> input, int im_width, int im_height);
-int minimum(std::vector <int> *grayscale_image);
-int maximum(std::vector <int> *grayscale_image);
+std::vector<int> generate_im(int im_width, int im_height);
+std::vector<int> contrast_increase(std::vector<int> input, int im_width, int im_height);
+int minimum(std::vector<int> *grayscale_image);
+int maximum(std::vector<int> *grayscale_image);
 int use_formula(int value, int max_value, int min_value);
 
 #endif  // MODULES_TASK_1_MITYAGINA_D_INCREASING_THE_CONTRAST_INCREASING_THE_CONTRAST_H_
