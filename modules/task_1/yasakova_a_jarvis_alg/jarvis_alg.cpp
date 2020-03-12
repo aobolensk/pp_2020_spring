6e326f72773947503c3ddc8e834647463e8ec32c
yasakova-anastasiya <yasakova_anastasiya@mail.ru>
2020-03-12 22:51:42 +0300

    fixed3

--- modules/task_1/yasakova_a_jarvis_alg/jarvis_alg.cpp
+++ modules/task_1/yasakova_a_jarvis_alg/jarvis_alg.cpp
@@ -28,7 +28,7 @@ std::vector < std::pair<int, int>> JarvisAlg(const std::vector<std::pair<int, in
     while (tmp != res[0]) {
         res.push_back(tmp);
         double min_cos = 1;
-        for (auto& val : points) {
+        for (const auto& val : points) {
             double curr_cos = CountCos(res[res.size() - 1], res[res.size() - 2], val);
             if (curr_cos < min_cos) {
                 min_cos = curr_cos;
@@ -43,7 +43,7 @@ std::vector < std::pair<int, int>> JarvisAlg(const std::vector<std::pair<int, in
 
 std::pair<int, int> FindFirstPoint(const std::vector<std::pair<int, int>>& points) {
     std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
-    for (auto& val : points) {
+    for (const auto& val : points) {
         if (val.second < res.second)
            res = std::make_pair(val.first, val.second);
         else if (val.second == res.second && val.first < res.first)
@@ -55,7 +55,7 @@ std::pair<int, int> FindFirstPoint(const std::vector<std::pair<int, int>>& point
 std::pair<int, int> FindSecondPoint(const std::vector<std::pair<int, int>>& points, std::pair<int, int> tmp) {
     std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
     double min = 4;
-    for (auto& val : points) {
+    for (const auto& val : points) {
         if (val != tmp) {
             double angle = atan(static_cast<double>(val.second - tmp.second)
                / static_cast<double>(abs(val.first - tmp.first)));
