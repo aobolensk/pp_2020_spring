ea6b7669ea6cf1dc0a91cb27676b770933a3e4cc
3817061LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2020-03-13 21:03:07 +0300

    add 4 and 5 tests

--- modules/task_2/lembrikov_s_jarvis_omp/main.cpp
+++ modules/task_2/lembrikov_s_jarvis_omp/main.cpp
@@ -70,49 +70,33 @@ TEST(Vector_Min_MPI, Test_On_2_Equal_Points_EQ) {
     size_t size = 2;
     size_t size_a = 1;
     int num_thr = 2;
-    std::vector<std::pair<double, double>> answer_func(size);
-    std::vector<std::pair<double, double>> answer_right(size_a);
+    std::vector<std::pair<double, double>> answer_omp;
+    std::vector<std::pair<double, double>> answer_seq;
     std::vector<std::pair<double, double>> pr(size);
 
     pr[0].first = 3;
     pr[0].second = 4;
     pr[1].first = 3;
     pr[1].second = 4;
-    answer_right[0].first = 3;
-    answer_right[0].second = 4;
-    answer_func = Jarvis_Omp(pr, num_thr);
+    answer_omp = Jarvis_Omp(pr, num_thr);
+    answer_seq = Jarvis_Seq(pr);
 
-    EXPECT_EQ(answer_func, answer_right);
+    EXPECT_EQ(answer_omp, answer_seq);
 }
 
 TEST(Vector_Min_MPI, Test_On_Random_Points_EQ) {
     std::vector<std::pair<double, double>> a;
-    std::vector<std::pair<double, double>> answer;
-    size_t size;
-    size_t size_a = 2;
-    int num_thr = 2;
-
-    a = getRandomPoints(size_a);
-    answer = Jarvis_Omp(a, num_thr);
-    size = answer.size();
-
-    EXPECT_EQ(size_a, size);
-}
-
-TEST(Vector_Min_MPI, Test_On_Random_Points_EQ_123123123111111111111111) {
-    std::vector<std::pair<double, double>> a;
     std::vector<std::pair<double, double>> answer_seq;
-    std::vector<std::pair<double, double>> answer_par;
+    std::vector<std::pair<double, double>> answer_omp;
     size_t size;
-    size_t size_a = 100;
+    size_t size_a = 10;
     int num_thr = 2;
 
     a = getRandomPoints(size_a);
     answer_seq = Jarvis_Seq(a);
-    answer_par = Jarvis_Omp(a, num_thr);
-    //size = answer.size();
+    answer_omp = Jarvis_Omp(a, num_thr);
 
-    EXPECT_EQ(answer_seq, answer_par);
+    EXPECT_EQ(answer_seq, answer_omp);
 }
 
 TEST(Vector_Min_MPI, Test_On_Random_Points_NE) {
