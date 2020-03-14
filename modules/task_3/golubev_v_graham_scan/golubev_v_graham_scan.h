d46bdec268f79bbebee7f6da74fdc391f577eaf4
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-03-14 14:55:31 +0300

    add sequential scan

--- modules/task_3/golubev_v_graham_scan/golubev_v_graham_scan.h
+++ modules/task_3/golubev_v_graham_scan/golubev_v_graham_scan.h
@@ -4,5 +4,37 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <tbb/tbb.h>
+#include <iostream>
+using namespace tbb;
+
+// Generate area
+std::vector<std::pair<double, double> > get_rand_set(std::size_t size);
+std::vector<std::pair<double, double> > get_test_set_1();
+std::vector<std::pair<double, double> > get_test_set_2();
+std::vector<std::pair<double, double> > get_test_set_3();
+
+// Sequential graham_scan
+bool is_less(const std::pair<double, double>& a, const std::pair<double, double>& b);
+
+double get_polar_r(const std::pair<double, double>& point);
+double get_polar_grad(const std::pair<double, double>& point);
+double get_det(const std::pair<double, double>& x,
+  const std::pair<double, double>& y, const std::pair<double, double>& z);
+std::size_t get_lex_min(std::vector<std::pair<double, double> > v);
+
+std::vector<std::pair<double, double> > graham_scan(std::vector<std::pair<double, double> >::iterator begin,
+  std::vector<std::pair<double, double> >::iterator end);
+
+// TBB class_for_graham_scan
+
+class TBB_scan {
+  std::vector<std::pair<double, double> > area;
+  std::vector<std::pair<double, double> > scan;
+
+public:
+  TBB_scan(std::vector<std::pair<double, double> > _area);
+  void operator() (const blocked_range<std::vector<std::pair<double, double> >::iterator >& range) const;
+};
 
 #endif  // MODULES_TASK_3_GOLUBEV_V_GRAHAM_SCAN_GOLUBEV_V_GRAHAM_SCAN_H_
--- modules/task_3/golubev_v_graham_scan/main.cpp
+++ modules/task_3/golubev_v_graham_scan/main.cpp
@@ -1,7 +1,33 @@
 // Copyright 2020 Golubev Vladislav
 #include <gtest/gtest.h>
 #include <vector>
+#include <iostream>
+#include <tbb/tbb.h>
+#include <stdlib.h>
+#include "../../../modules/task_3/golubev_v_graham_scan/golubev_v_graham_scan.h"
+using namespace tbb;
 
+TEST(test, DISABLED_test) {
+  std::size_t size = 10;
+  auto area = get_rand_set(size);
+  std::vector<std::pair<double, double> > check(size);
+  TBB_scan scan(area);
+}
+
+TEST(test, scheduler) {
+  task_scheduler_init init(5);
+  task_group g;
+  for (int i = 0; i < 5; ++i) {
+    g.run([] {Sleep(1000); });
+  }
+  g.wait();
+}
+
+TEST(test, without_scheduler) {
+  for (int i = 0; i < 5; ++i) {
+    Sleep(1000);
+  }
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
