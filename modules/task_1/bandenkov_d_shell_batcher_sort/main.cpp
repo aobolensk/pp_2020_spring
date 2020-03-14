fed343bf8797c2821c4e2d9bbd16cdad7c29c900
381706-2BandenkovD <daniilbandenkov@gmail.com>
2020-03-14 20:21:13 +0300

    fi

--- modules/task_1/bandenkov_d_shell_batcher_sort/main.cpp
+++ modules/task_1/bandenkov_d_shell_batcher_sort/main.cpp
@@ -55,15 +55,5 @@ TEST(test_Shell_Betch_sort, test_on_10000_elements) {
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    MPI_Init(&argc, &argv);
-
-    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-    ::testing::TestEventListeners& listeners =
-        ::testing::UnitTest::GetInstance()->listeners();
-
-    listeners.Release(listeners.default_result_printer());
-    listeners.Release(listeners.default_xml_generator());
-
-    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
     return RUN_ALL_TESTS();
 }
