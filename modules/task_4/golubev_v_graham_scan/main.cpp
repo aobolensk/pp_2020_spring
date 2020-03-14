2a4a02ad6d41794f7484ee4ccbc4b3c183d21d8c
381706-1Golubev <Vladgolube1999@rambler.ru>
2020-03-14 17:57:57 +0300

    add sequential scan

---
+++ modules/task_4/golubev_v_graham_scan/main.cpp
@@ -0,0 +1,16 @@
+// Copyright 2020 Golubev Vladislav
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <stack>
+#include "./golubev_v_graham_scan.h"
+
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
