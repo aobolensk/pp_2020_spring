873a3096bc0514462611748be0a226405d28ecd2
KornevNikita <copperskylark@gmail.com>
2020-03-14 16:08:31 +0300

    add qs_omp()

--- modules/task_2/kornev_n_qs/main.cpp
+++ modules/task_2/kornev_n_qs/main.cpp
@@ -3,61 +3,58 @@
 #include <utility>
 #include <algorithm>
 #include "gtest/gtest.h"
-#include "../../../modules/task_1/kornev_n_qs/qs.h"
+#include "../../../modules/task_2/kornev_n_qs/qs.h"
 
-TEST(seq_qs, corr_res) {
+TEST(omp_qs, corr_res) {
+  int n = 100000;
+  double* a = new double[n], * b = new double[n];
+  get_rand_arr(a, n);
+  arrcpy(a, b, n);
+  qs(b, 0, n - 1);
+  qs_omp(a, n);
+  bool res = std::equal(&a[0], &a[n], &b[0]);
+  ASSERT_EQ(res, 1);
+}
+
+TEST(omp_qs, sorted_arr) {
   int n = 100000;
   double* a = new double[n], *b = new double[n];
   get_rand_arr(a, n);
-
-  arrcpy(a, b, n);
-  std::sort(&b[0], &b[n]);
   qs(a, 0, n - 1);
+  arrcpy(a, b, n);
+  qs_omp(b, n);
 
   bool res = std::equal(&a[0], &a[n], &b[0]);
   ASSERT_EQ(res, 1);
 }
 
-TEST(seq_qs, sorted_arr) {
+TEST(omp_qs, inverted_arr) {
   int n = 100000;
-  double* a = new double[n], *b = new double[n];
+  double* a = new double[n], * b = new double[n];
   get_rand_arr(a, n);
 
-  std::sort(&a[0], &a[n]);
-  arrcpy(a, b, n);
   qs(a, 0, n - 1);
-
-  bool res = std::equal(&a[0], &a[n], &b[0]);
-  ASSERT_EQ(res, 1);
-}
-
-TEST(seq_qs, inverted_arr) {
-  int n = 100000;
-  double* a = new double[n], *b = new double[n];
-  get_rand_arr(a, n);
-
-  std::sort(&a[0], &a[n]);
   arrcpy(a, b, n);
 
   for (int i = 0; i < n / 2; i++) {
     std::swap(a[i], a[n - i - 1]);
   }
 
-  qs(a, 0, n - 1);
+  qs_omp(a, n);
 
   bool res = std::equal(&a[0], &a[n], &b[0]);
   ASSERT_EQ(res, 1);
 }
 
-TEST(seq_qs, almost_empty_arr) {
+TEST(omp_qs, almost_empty_arr) {
   int n = 100000;
   double* a = new double[n];
   a[0] = 1;
-  qs(a, 0, n - 1);
+  qs_omp(a, n);
   ASSERT_EQ(a[n - 1], 1);
 }
 
-TEST(seq_qs, empty_arr) {
+TEST(omp_qs, empty_arr) {
   int n = 100000;
   double* a = new double[n];
 
@@ -65,7 +62,7 @@ TEST(seq_qs, empty_arr) {
     a[i] = 0;
   }
 
-  qs(a, 0, n - 1);
+  qs_omp(a, n);
 
   double sum = std::accumulate(&a[0], &a[n], 0);
 
--- modules/task_2/kornev_n_qs/qs.cpp
+++ modules/task_2/kornev_n_qs/qs.cpp
@@ -1,6 +1,8 @@
 // Copyright 2020 Kornev Nikita
+#include <omp.h>
 #include <utility>
-#include "../../../modules/task_1/kornev_n_qs/qs.h"
+#include <vector>
+#include "../../../modules/task_2/kornev_n_qs/qs.h"
 
 void qs(double* a, int first, int last) {
   int i = first, j = last;
@@ -27,7 +29,7 @@ void qs(double* a, int first, int last) {
 }
 
 void get_rand_arr(double* a, int n) {
-  std::mt19937 gen(time(0));
+  std::mt19937 gen(static_cast<unsigned int>(time(0)));
   std::uniform_int_distribution<> uid(0, n);
   for (int i = 0; i < n; i++) {
     a[i] = uid(gen);
@@ -37,5 +39,72 @@ void get_rand_arr(double* a, int n) {
 void arrcpy(double* a, double* b, int n) {
   for (int i = 0; i < n; i++) {
     b[i] = a[i];
+  }
+}
+
+void merge(double* a, int n, double* b, int m) {
+  int i = 0, j = 0, k = 0;
+  double* temp = new double[n + m];
+  while (i < n && j < m) {
+    if (a[i] > b[j]) {
+      temp[k++] = b[j++];
+    } else {
+      if (a[i] < b[j]) {
+        temp[k++] = a[i++];
+      } else {
+        temp[k++] = a[i++];
+        temp[k++] = b[j++];
+      }
+    }
+  }
+
+  while (i < n) {
+    temp[k++] = a[i++];
+  }
+
+  while (j < m) {
+    temp[k++] = b[j++];
+  }
+
+  for (int i = 0; i < m + n; i++) {
+    a[i] = temp[i];
+  }
+}
+
+void qs_omp(double* a, int n) {
+  int num_threads = 4;  // define num of threads
+  omp_set_num_threads(num_threads);
+  std::vector<std::vector<int>> start_fin(num_threads, std::vector<int>(2));
+  // [thread][0] & [thread][1] - start & finish of mini-arrays that will be sort by each thread
+
+#pragma omp parallel  // sort
+  {
+    int thread = omp_get_thread_num();
+    start_fin[thread][0] = thread * n / num_threads;  // define start of mini-array that will be sort by this thread
+
+    if (thread == num_threads - 1) {
+      start_fin[thread][1] = n - 1;  // define fin
+    } else {
+      start_fin[thread][1] = start_fin[thread][0] + n / num_threads - 1;  // or this fin
+    }
+
+    qs(a, start_fin[thread][0], start_fin[thread][1]);
+  }
+
+  int m = num_threads / 2;  // num of pairs that will be merge by some threads
+  while (start_fin.size() > 1) {
+    omp_set_num_threads(m);
+#pragma omp parallel
+    {
+      size_t thread = omp_get_thread_num();
+      merge(&a[start_fin[thread * 2][0]], start_fin[thread * 2][1] - start_fin[thread * 2][0] + 1,
+        &a[start_fin[thread * 2 + 1][0]], start_fin[thread * 2 + 1][1] - start_fin[thread * 2 + 1][0] + 1);
+      start_fin[thread * 2][1] = start_fin[thread * 2 + 1][1];  // fin[i] = fin[i + 1]
+    }
+    for (int i = 1; i <= m; i++) {
+      start_fin.erase(start_fin.begin() + i);  // fin[i] = fin[i + 1] then delete start_fin[i + 1]
+    }
+    num_threads -= m;
+    m = static_cast<int>(num_threads) / 2;
   }
 }
