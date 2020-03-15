b6ccaacc8b130ba3fe416751719abd537cf19d10
black8cherry <art8evd@gmail.com>
2020-03-15 22:17:26 +0300

    test added

--- modules/task_1/evdokimov_a_image_filtering/main.cpp
+++ modules/task_1/evdokimov_a_image_filtering/main.cpp
@@ -3,6 +3,30 @@
 #include <vector>
 #include "./image_filtering.h"
 
+TEST(Filtering_Image, Non_Quadratic_Image_Filtering_Correct_Work) {
+  // Arrange
+  int columns = 3;
+  int rows = 5;
+  // Act
+  std::vector<int> result = imageFiltering(
+      getTempImage({1, 2, 3, 
+                    4, 5, 6,
+                    7, 8, 9,
+                    8, 7, 6,
+                    5, 4, 3}, rows, columns), 
+      cernel(), 
+      rows, 
+      columns);
+  // Result
+  std::vector<int> expectedResult = {-11, -7, -3,
+                                     1, 5, 9,
+                                     12, 20, 28,
+                                     21, 13, 5, 
+                                     -1, -5, -9};
+  // Assert
+  ASSERT_EQ(result, expectedResult);
+}
+
 TEST(Filtering_Image, Image_Filtering_Correct_Work) {
   // Arrange
   int columns = 2;
