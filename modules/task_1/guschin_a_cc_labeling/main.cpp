4b199e6fe542a035bec4ddac8b2e4404902fec46
Alexander Guschin <igushin1999@gmail.com>
2020-03-17 15:51:42 +0300

    fix for tests

--- modules/task_1/guschin_a_cc_labeling/main.cpp
+++ modules/task_1/guschin_a_cc_labeling/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "../../../modules/task_1/guschin_a_cc_labeling/cc_labeling.h"
 
-TEST(Sequential, No_Throw) {
+TEST(CC_Labeling, No_Throw) {
   std::vector<std::vector<std::int8_t>> A(3, std::vector<std::int8_t>(3));
   A = {
     {1, 0, 1},
@@ -13,7 +13,7 @@ TEST(Sequential, No_Throw) {
   ASSERT_NO_THROW(Labeling(A));
 }
 
-TEST(Sequential, Can_Lable_Pic) {
+TEST(CC_Labeling, Can_Lable_Pic) {
   std::vector<std::vector<std::int8_t>> A(3, std::vector<std::int8_t>(3));
   A = {
     {1, 0, 1},
@@ -29,18 +29,57 @@ TEST(Sequential, Can_Lable_Pic) {
   EXPECT_EQ(Labeling(A), res);
 }
 
-TEST(Sequential, Can_Lable_Another_Pic) {
+TEST(CC_Labeling, Can_Lable_Another_Pic) {
   std::vector<std::vector<std::int8_t>> A(4, std::vector<std::int8_t>(4));
   A = {
-    {1, 1, 0, 1}, 
+    {1, 1, 0, 1},
     {0, 1, 1, 0},
     {1, 0, 1, 1},
-    {0, 1, 1, 0}};
+    {0, 1, 1, 0}
+  };
   std::vector<std::vector<int>> res(4, std::vector<int>(4));
   res = {
-    {1, 1, 0, 2}, 
+    {1, 1, 0, 2},
     {0, 1, 1, 0},
     {3, 0, 1, 1},
-    {0, 1, 1, 0}};
+    {0, 1, 1, 0}
+  };
   EXPECT_EQ(Labeling(A), res);
+}
+
+TEST(CC_Labeling, Can_Lable_Pic_With_Different_Size) {
+  std::vector<std::vector<std::int8_t>> A(4, std::vector<std::int8_t>(2));
+  A = {
+    {1, 1},
+    {0, 1},
+    {1, 0},
+    {1, 1}
+  };
+  std::vector<std::vector<int>> res(4, std::vector<int>(2));
+  res = {
+    {1, 1},
+    {0, 1},
+    {2, 0},
+    {2, 2}
+  };
+  EXPECT_EQ(Labeling(A), res);
+}
+
+TEST(CC_Labeling, Can_Merge) {
+  std::vector<std::vector<int>> A(4, std::vector<int>(4));
+  A = {
+    {3, 3, 0, 0},
+    {0, 3, 0, 0},
+    {4, 3, 0, 0},
+    {0, 0, 0, 0}
+  };
+  std::vector<std::vector<int>> res(4, std::vector<int>(4));
+  res = {
+    {3, 3, 0, 0},
+    {0, 3, 0, 0},
+    {3, 3, 0, 0},
+    {0, 0, 0, 0}
+  };
+  Merge(&A, 4, 3, 4, 4);
+  EXPECT_EQ(A, res);
 }
