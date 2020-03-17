75d0d6baf8c110f02cf57fc8bd83567b8fef3a93
Vlad Fedotov <fedotovvlad@list.ru>
2020-03-17 21:17:17 +0300

    Initial commit

---
+++ modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.cpp
@@ -0,0 +1,2 @@
+// Copyright 2020 Fedotov Vladislav
+#include "../../modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.h"
---
+++ modules/task_1/fedotov_v_multidimensional_integrals_rectangle/main.cpp
@@ -0,0 +1,4 @@
+// Copyright 2020 Fedotov Vladislav
+#include <gtest/gtest.h>
+#include <omp.h>
+#include "./integrals-rectangle.h"
