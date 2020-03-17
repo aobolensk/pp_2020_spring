ee1c4d7b714c1e52db8e7ddd6c1417167691936c
megarungle <megarungle@gmail.com>
2020-03-18 02:00:42 +0300

    Filter implementation

--- modules/task_1/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
+++ modules/task_1/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
@@ -2,4 +2,14 @@
 #ifndef MODULES_TASK_1_UTKIN_K_LIN_IMG_FILTER_GAUSS_VERT_LIN_IMG_FILTER_GAUSS_VERT_H_
 #define MODULES_TASK_1_UTKIN_K_LIN_IMG_FILTER_GAUSS_VERT_LIN_IMG_FILTER_GAUSS_VERT_H_
 
+#include <vector>
+
+std::vector<int> kernel = { 1, 2, 1, 2, 4, 2, 1, 2, 1 };
+int divKernel = 16;
+
+std::vector<std::vector<int>> getRandomPic(int n, int m);
+
+std::vector<std::vector<int>> addBorders(std::vector<std::vector<int>> pic, int n, int m);
+std::vector<std::vector<int>> gaussFilter(std::vector<std::vector<int>> pic, int n, int m);
+
 #endif  // MODULES_TASK_1_UTKIN_K_LIN_IMG_FILTER_GAUSS_VERT_LIN_IMG_FILTER_GAUSS_VERT_H_
