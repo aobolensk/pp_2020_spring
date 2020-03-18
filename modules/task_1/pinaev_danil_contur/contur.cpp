1f2a86a967124484f2e01d446ba3a106aafddeeb
DanPina <drobrorobro@gmail.com>
2020-03-18 08:55:10 +0300

    Last I hope

--- modules/task_1/pinaev_danil_contur/contur.cpp
+++ modules/task_1/pinaev_danil_contur/contur.cpp
@@ -1,4 +1,5 @@
-// Copyright 2020 Obolenskiy Arseniy
+// Copyright 2020 Pinaev Danil
+
 #include <algorithm>
 #include <cmath>
 #include <ctime>
@@ -6,6 +7,8 @@
 #include <random>
 #include <stdexcept>
 #include <vector>
+#include <iostream>
+#include <string>
 #include "../../modules/task_1/pinaev_danil_contur/contur.h"
 
 static int offset = 0;
@@ -16,14 +19,17 @@ Image generateRandomImage(int rows, int cols) {
 
     std::mt19937 gen;
     gen.seed((unsigned)time(0) + ++offset);
-    std::uniform_int_distribution<uint8_t> dis(0, 1);
+    std::uniform_int_distribution<uint8_t> dis(0, 255);
 
     Image res(rows + 2, cols + 2);
 
     for (int i = 1; i < rows + 1; ++i) {
-        for (int j = 1; j < cols + 1; ++i)
-            res.data[i * (rows + 2) + j] = static_cast<uint8_t>(dis(gen));
+        for (int j = 1; j < cols + 1; ++j) {
+            if (static_cast<uint8_t>(dis(gen)) > 200)
+                res.data[i * (cols + 2) + j] = 0;
+        }
     }
+
     return res;
 }
 
@@ -33,42 +39,68 @@ Image getContur(const Image &in) {
     int cols = in.cols,
         rows = in.rows;
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            // Main pixel index is [i][j] (rows * i + j) now
-            // Now wee need to check all neighboring pixels
+    //   _________________
+    //  |     |     |     |
+    //  |  1  |  2  |  3  |
+    //  | ___ | ___ | ___ |
+    //  |  4  |     |  6  |
+    //  | ___ | ___ | ___ |
+    //  |  7  |  8  |  0  |
+    //  |_____|_____|_____|
+    //
 
-            //   _________________
-            //  |     |     |     |       
-            //  |  1  |  2  |  3  |
-            //  | ___ | ___ | ___ |
-            //  |  4  |     |  6  |
-            //  | ___ | ___ | ___ |
-            //  |  7  |  8  |  0  |
-            //  |_____|_____|_____|
-            //
+    // First is anchor
 
-            // 1  - (i + 0)*(cols + 2) + (j + 0)
-            // 2  - (i + 0)*(cols + 2) + (j + 1)
-            // 3  - (i + 0)*(cols + 2) + (j + 2)
-            // 4  - (i + 1)*(cols + 2) + (j + 0)
-            // 5  - main -  (i + 1)*(cols + 2) + (j + 1)
-            // 6  - (i + 1)*(cols + 2) + (j + 2)
-            // 7  - (i + 2)*(cols + 2) + (j + 0)
-            // 8  - (i + 2)*(cols + 2) + (j + 1)
-            // 9  - (i + 2)*(cols + 2) + (j + 2)
+    // 1  - (i + 0)*(cols + 2) + (j + 0)
+    // 2  - (i + 0)*(cols + 2) + (j + 1)
+    // 3  - (i + 0)*(cols + 2) + (j + 2)
+    // 4  - (i + 1)*(cols + 2) + (j + 0)
+    // 5  - main -  (i + 1)*(cols + 2) + (j + 1)
+    // 6  - (i + 1)*(cols + 2) + (j + 2)
+    // 7  - (i + 2)*(cols + 2) + (j + 0)
+    // 8  - (i + 2)*(cols + 2) + (j + 1)
+    // 9  - (i + 2)*(cols + 2) + (j + 2)
 
-            if (in.data[(i + 1)*(cols + 2) + (j + 1)] == 255)
-                if( in.data[(i + 0)*(cols + 2) + (j + 0)] == 0 ||
-                    in.data[(i + 0)*(cols + 2) + (j + 1)] == 0 ||
-                    in.data[(i + 0)*(cols + 2) + (j + 2)] == 0 ||
-                    in.data[(i + 1)*(cols + 2) + (j + 0)] == 0 ||
-                    in.data[(i + 1)*(cols + 2) + (j + 2)] == 0 ||
-                    in.data[(i + 2)*(cols + 2) + (j + 0)] == 0 ||
-                    in.data[(i + 2)*(cols + 2) + (j + 1)] == 0 ||
-                    in.data[(i + 2)*(cols + 2) + (j + 2)] == 0
-                ) res.data[(i + 1)*(cols + 2) + (j + 1)] = 127;
+    for (int i = 0; i < rows - 2; ++i) {
+        // Main pixel index is [i][j] (rows * i + j) now
+        // Now wee need to check all neighboring pixels
+        for (int j = 0; j < cols - 2; ++j) {
+            if (in.data[(i + 1)*(cols) + (j + 1)] == 255) {
+                if (in.data[(i + 0)*(cols) + (j + 0)] == 0 ||
+                    in.data[(i + 0)*(cols) + (j + 1)] == 0 ||
+                    in.data[(i + 0)*(cols) + (j + 2)] == 0 ||
+                    in.data[(i + 1)*(cols) + (j + 0)] == 0 ||
+                    in.data[(i + 1)*(cols) + (j + 2)] == 0 ||
+                    in.data[(i + 2)*(cols) + (j + 0)] == 0 ||
+                    in.data[(i + 2)*(cols) + (j + 1)] == 0 ||
+                    in.data[(i + 2)*(cols) + (j + 2)] == 0) {
+                    res.data[(i + 1)*(cols) + (j + 1)] = 127;
+                }
+            }
         }
     }
+
     return res;
+}
+
+void print(std::string mes, const Image &img) {
+    int real_rows = img.rows;
+    int real_cols = img.cols;
+
+    std::cout<< mes <<std::endl;
+
+    if (real_rows * real_cols != static_cast<int>(img.data.size())) {
+        std::cout << "real_rows * real_cols: "<< real_rows * real_cols
+                  << " img.data.size(): " << img.data.size()<< std::endl
+                  << "that is all" << std::endl;
+
+        throw std::runtime_error("Alarm!!");
+    }
+
+
+    for (int i = 0; i < real_cols; ++i) {
+        for (int j = 0; j < real_rows; ++j)
+            std::cout<< img.data[i * real_cols + j]<< ", ";
+        std::cout << std::endl;
+    }
 }
--- modules/task_1/pinaev_danil_contur/main.cpp
+++ modules/task_1/pinaev_danil_contur/main.cpp
@@ -1,17 +1,11 @@
 // Copyright 2020 Pinaev Danil
+
 #include <gtest/gtest.h>
 #include <omp.h>
+
+#include <iostream>
+
 #include "./contur.h"
-
-TEST(Contur_ceq, Can_Take_Contur) {
-    const int width = 10;
-    const int height = 10;
-
-    Image img = generateRandomImage(width, height);
-    Image res = getContur(img);
-
-    ASSERT_NE(res.data, img.data);
-}
 
 TEST(Contur_ceq, Can_Make_One_Pixel_Image) {
     const int width = 1;
@@ -30,14 +24,19 @@ TEST(Contur_ceq, Image_Size_Should_Be_Positive) {
     ASSERT_ANY_THROW(generateRandomImage(width, height));
 }
 
-TEST(Contur_ceq, Can_Get_Contur) {
-    const int width = 50;
-    const int height = 50;
+TEST(Contur_ceq, Can_Take_Contur_Rand) {
+    const int width = 10;
+    const int height = 10;
 
     Image img = generateRandomImage(width, height);
+
     Image res = getContur(img);
 
-    ASSERT_NE(img.data, res.data);
+    print("img", img);
+
+    print("res", res);
+
+    ASSERT_NE(res.data, img.data);
 }
 
 TEST(Contur_ceq, Can_Get_Contur_Weird_Size_1) {
@@ -45,14 +44,15 @@ TEST(Contur_ceq, Can_Get_Contur_Weird_Size_1) {
     const int height = 1;
 
     Image img = generateRandomImage(width, height);
+
     Image res = getContur(img);
 
     ASSERT_NE(img.data, res.data);
 }
 
 TEST(Contur_ceq, Can_Get_Contur_Weird_Size_2) {
-    const int width = 2;
-    const int height = 1234;
+    const int width = 1;
+    const int height = 1000;
 
     Image img = generateRandomImage(width, height);
     Image res = getContur(img);
