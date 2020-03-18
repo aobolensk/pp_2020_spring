6fa89eb1d0fb49a8a8d30d0b2d3cf06776781816
381706-2KDA2000 <43183898+381706-2KDA2000@users.noreply.github.com>
2020-03-19 00:05:57 +0300

    Kriukov D strassen multiplication

--- modules/task_1/kriukov_strassen_algorithm/strassen_algorithm.cpp
+++ modules/task_1/kriukov_strassen_algorithm/strassen_algorithm.cpp
@@ -1,72 +1,83 @@
-#include "strassen_algorithm.h"
+// Copyright 2020 Kriukov Dmitry
+
+#include <../../../modules/task_1/kriukov_strassen_algorithm/strassen_algorithm.h>
 #include <random>
 #include <ctime>
+#include <vector>
 
 std::vector<double> getRandomMatrix(int sz) {
   std::mt19937 gen;
   gen.seed(static_cast<unsigned int>(time(0)));
-  std::vector<double> vec(sz);
-  for (int i = 0; i < sz; i++) {
-    vec[i] = gen() % 99 + 1;
+  std::vector<double> vec(sz*sz);
+  for (int i = 0; i < sz*sz; i++) {
+    vec[i] = gen() % 9 + 1;
   }
   return vec;
 }
 
-std::vector<double> regularMultiplication(std::vector<double>& a, std::vector<double>& b)
-{
-  int n = static_cast<int>(sqrt(a.size()));
-  std::vector<double> res(a.size());
-  for(int i = 0; i < n; i++)
-    for (int j = 0; j < n; j++){
-      for (int k = 0; k < n; k++)
-        res[j + i * n] += a[i * n + k] * b[j + k * n];
+std::vector<double> regularMultiplication(const std::vector<double>& a, const std::vector<double>& b, unsigned int sz) {
+  std::vector<double> res(sz* sz);
+  for (unsigned int i = 0; i < sz; i++)
+    for (unsigned int j = 0; j < sz; j++) {
+      for (unsigned int k = 0; k < sz; k++)
+        res[j + i * sz] += a[i * sz + k] * b[j + k * sz];
     }
   return res;
 }
 
-std::vector<double> regularSum(std::vector<double>& a, std::vector<double>& b, int sz)
-{
-  std::vector<double> res(sz);
-  for (int i = 0; i < sz; i++) {
+std::vector<double> sum(const std::vector<double>& a, const std::vector<double>& b, unsigned int sz) {
+  unsigned int size = sz * sz;
+  std::vector<double> res(size);
+  for (int i = 0; i < size; i++) {
     res[i] = a[i] + b[i];
   }
   return res;
 }
 
-void splitMatrix(std::vector<double> &mtx, std::vector<double> &a, std::vector<double> &b, std::vector<double> &c, std::vector<double> &d, int size) {
-  unsigned int sz = static_cast<unsigned int>(sqrt(size/4));
+std::vector<double> diff(const std::vector<double>& a, const std::vector<double>& b, unsigned int sz) {
+  unsigned int size = sz * sz;
+  std::vector<double> res(size);
+  for (int i = 0; i < size; i++) {
+    res[i] = a[i] - b[i];
+  }
+  return res;
+}
+
+void splitMatrix(const std::vector<double> &mtx, std::vector<double> *a,
+                 std::vector<double> *b, std::vector<double> *c, std::vector<double> *d, unsigned int size) {
+  unsigned int sz = size / 2;
   for (unsigned int i = 0; i < sz; i++) {
     for (unsigned int j = i * 2 * sz; j < i * 2 * sz + sz; j++)
-      a.push_back(mtx[j]);
+      a->push_back(mtx[j]);
     for (unsigned int j = i * 2 * sz + sz; j < i * 2 * sz + 2 * sz; j++)
-      b.push_back(mtx[j]);
+      b->push_back(mtx[j]);
   }
   for (unsigned int i = sz; i < 2*sz; i++) {
     for (unsigned int j = i * 2 * sz; j < i * 2 * sz + sz; j++)
-      c.push_back(mtx[j]);
+      c->push_back(mtx[j]);
     for (unsigned int j = i * 2 * sz + sz; j < i * 2 * sz + 2 * sz; j++)
-      d.push_back(mtx[j]);
+      d->push_back(mtx[j]);
   }
 }
 
-void mergeMatrix(std::vector<double> &mtx, std::vector<double> &a, std::vector<double> &b, std::vector<double> &c, std::vector<double> &d, int size) {
-  unsigned int sz = static_cast<unsigned int>(sqrt(size / 4));
+void mergeMatrix(std::vector<double> *mtx, const std::vector<double> &a, const std::vector<double> &b,
+                 const std::vector<double> &c, const std::vector<double> &d, unsigned int size) {
+  unsigned int sz = size / 2;
   for (unsigned int i = 0; i < sz; i++) {
     for (unsigned int j = i * sz; j < i * sz + sz; j++)
-      mtx.push_back(a[j]);
+      mtx->push_back(a[j]);
     for (unsigned int j = i * sz; j < i * sz + sz; j++)
-      mtx.push_back(b[j]);
+      mtx->push_back(b[j]);
   }
   for (unsigned int i = 0; i < sz; i++) {
     for (unsigned int j = i * sz; j < i * sz + sz; j++)
-      mtx.push_back(c[j]);
+      mtx->push_back(c[j]);
     for (unsigned int j = i * sz; j < i * sz + sz; j++)
-      mtx.push_back(d[j]);
+      mtx->push_back(d[j]);
   }
 }
 
-std::vector<double> toPowerOfTwoSize(std::vector<double> &mtx) {
-  unsigned int sz = static_cast<unsigned int>(sqrt(mtx.size()));
+std::vector<double> toPowerOfTwoSize(const std::vector<double> &mtx, unsigned int sz) {
   unsigned int power = 2;
   while (sz > power)
     power *= 2;
@@ -81,5 +92,58 @@ std::vector<double> toPowerOfTwoSize(std::vector<double> &mtx) {
     for (unsigned j = 0; j < power; j++)
       res.push_back(0);
   }
+  return res;
+}
+
+std::vector<double> strassenMultiplication(const std::vector<double>& a,
+                                           const std::vector<double>& b, unsigned int sz) {
+  std::vector<double> a_ = toPowerOfTwoSize(a, sz);
+  std::vector<double> b_ = toPowerOfTwoSize(b, sz);
+
+  return strassenMultiplication_NoCastToPowerOfTwo(a_, b_, sz);
+}
+
+std::vector<double> strassenMultiplication_NoCastToPowerOfTwo(const std::vector<double>& a_,
+                    const std::vector<double>& b_, unsigned int sz) {
+  std::vector<double> res;
+
+  std::vector<double> a11, a12, a21, a22;
+  std::vector<double> b11, b12, b21, b22;
+
+  splitMatrix(a_, &a11, &a12, &a21, &a22, sz);
+  splitMatrix(b_, &b11, &b12, &b21, &b22, sz);
+
+  unsigned int split_sz = sz / 2;
+
+  std::vector<double> p1, p2, p3, p4, p5, p6, p7;
+
+
+  if (a11.size() > 256) {
+    p1 = strassenMultiplication_NoCastToPowerOfTwo(sum(a11, a22, split_sz), sum(b11, b22, split_sz), split_sz);
+    p2 = strassenMultiplication_NoCastToPowerOfTwo(sum(a21, a22, split_sz), b11, split_sz);
+    p3 = strassenMultiplication_NoCastToPowerOfTwo(a11, diff(b12, b22, split_sz), split_sz);
+    p4 = strassenMultiplication_NoCastToPowerOfTwo(a22, diff(b21, b11, split_sz), split_sz);
+    p5 = strassenMultiplication_NoCastToPowerOfTwo(sum(a11, a12, split_sz), b22, split_sz);
+    p6 = strassenMultiplication_NoCastToPowerOfTwo(diff(a21, a11, split_sz), sum(b11, b12, split_sz), split_sz);
+    p7 = strassenMultiplication_NoCastToPowerOfTwo(diff(a12, a22, split_sz), sum(b21, b22, split_sz), split_sz);
+  } else {
+    p1 = regularMultiplication(sum(a11, a22, split_sz), sum(b11, b22, split_sz), split_sz);
+    p2 = regularMultiplication(sum(a21, a22, split_sz), b11, split_sz);
+    p3 = regularMultiplication(a11, diff(b12, b22, split_sz), split_sz);
+    p4 = regularMultiplication(a22, diff(b21, b11, split_sz), split_sz);
+    p5 = regularMultiplication(sum(a11, a12, split_sz), b22, split_sz);
+    p6 = regularMultiplication(diff(a21, a11, split_sz), sum(b11, b12, split_sz), split_sz);
+    p7 = regularMultiplication(diff(a12, a22, split_sz), sum(b21, b22, split_sz), split_sz);
+  }
+
+  std::vector<double> c11, c12, c21, c22;
+
+  c11 = sum(diff(sum(p1, p4, split_sz), p5, split_sz), p7, split_sz);
+  c12 = sum(p3, p5, split_sz);
+  c21 = sum(p2, p4, split_sz);
+  c22 = sum(sum(diff(p1, p2, split_sz), p3, split_sz), p6, split_sz);
+
+  mergeMatrix(&res, c11, c12, c21, c22, sz);
+
   return res;
 }
