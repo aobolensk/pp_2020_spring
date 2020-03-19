efcf4cca53752b8791738917a52651f074812a29
Vlad Fedotov <fedotovvlad@list.ru>
2020-03-20 02:30:52 +0300

    first version without tests

--- modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.cpp
+++ modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.cpp
@@ -1,2 +1,32 @@
 // Copyright 2020 Fedotov Vladislav
 #include "../../modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.h"
+
+double getMultipleIntegralUsingRectangleMethod(double (*function)(double, double), double x1, double x2, double y1, double y2, double stepX, double stepY) {
+	double resultIntegral = 0;
+	double cellSquare = stepX * stepY;
+
+
+	for (double i = x1; i < x2; i += stepX) {
+		for (double j = y1; j < y2; j += stepY) {
+			double xMiddle = (i + stepX) / 2;
+			double yMiddle = (j + stepY) / 2;
+
+			resultIntegral += function(xMiddle, yMiddle);
+		}
+	}
+
+	resultIntegral *= cellSquare; // common factor
+
+	return resultIntegral;
+}
+
+double function1(double x, double y) {
+	return x - y;
+}
+
+int main() {
+	double (*passingFunc)(double, double) = null;
+	passingFunc = &function1;
+
+	double integral = getMultipleIntegralUsingRectangleMethod(passingFunc, 0, 2, 0, 2, 0.1, 0.1);
+}
--- modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.h
+++ modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.h
@@ -2,5 +2,7 @@
 #ifndef MODULES_TASK_1_FEDOTOV_V_MULTIDIMENSIONAL_INTEGRALS_RECTANGLE_H_
 #define MODULES_TASK_1_FEDOTOV_V_MULTIDIMENSIONAL_INTEGRALS_RECTANGLE_H_
 
+double getMultipleIntegralUsingRectangleMethod(double (*function)(double, double), double x1, double x2, double y1, double y2, double stepX, double stepY);
+double function1(double x, double y);
 
 #endif  // MODULES_TASK_1_FEDOTOV_V_MULTIDIMENSIONAL_INTEGRALS_RECTANGLE_H_
