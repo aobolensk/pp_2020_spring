afd921ec36b986d00983a9ce6253e3039bc5c9a3
AleksandraSavosina <alexandra.savosina@gmail.com>
2020-03-19 13:07:07 +0300

    try 1

---
+++ modules/task_1/savosina_a_sobel/savosina_a.h
@@ -0,0 +1,74 @@
+// Copyright 2020 Savosina Aleksandra
+
+#ifndef MODULES_TASK_1_SAVOSINA_A_SOBEL_SAVOSINA_A_H_
+#define MODULES_TASK_1_SAVOSINA_A_SOBEL_SAVOSINA_A_H_
+
+
+#include <vector>
+#include <random>
+#include <iostream>
+
+struct img {
+  std::vector<int> pict;
+  int height;
+  int width;
+  int size;
+
+  img() {
+    height = width = size = 0;
+  }
+
+  img(int _height, int _width) {
+    height = _height;
+    width = _width;
+    size = height * width;
+    for (int i = 0; i < size; i++)
+      pict[i] = 0;
+  }
+
+  img(int _height, int _width, std::vector<int> _pict) {
+    height = _height;
+    width = _width;
+    size = height * width;
+    pict = _pict;
+  }
+
+  img(const img& _img) {
+    size = _img.size;
+    height = _img.height;
+    width = _img.width;
+    pict = _img.pict;
+  }
+  
+  /*void set(int tmp, int i) {
+    pict[i] = tmp;
+  }*/
+
+  bool operator== (const img& _img) const {
+    if (pict == _img.pict && height == _img.height
+      && width == _img.width && size == _img.size)
+      return true;
+    return false;
+  }
+
+  bool operator!= (const img& _img) const { 
+    return !(*this == _img);
+  }
+
+  void getRandom(int _height, int _width) {
+    height = _height;
+    width = _width;
+    size = height * width;
+    pict.resize(size);
+
+    for (int i = 0; i < size; i++)
+      pict[i] = rand() % 256 + 0;
+  }
+};
+
+using mainImage = img;
+using mask = img;
+
+mask sobel(const mask& _img);
+
+#endif  // MODULES_TASK_1_SAVOSINA_A_SOBEL_SAVOSINA_A_H_
