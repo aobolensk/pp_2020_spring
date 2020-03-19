a3282dd2410518827d0e70709baa2f330a8727fc
Vlad Fedotov <fedotovvlad@list.ru>
2020-03-20 02:51:01 +0300

    null -> nullptr

--- modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.cpp
+++ modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.cpp
@@ -1,10 +1,10 @@
 // Copyright 2020 Fedotov Vladislav
 #include "../../modules/task_1/fedotov_v_multidimensional_integrals_rectangle/integrals-rectangle.h"
+#include <iostream>
 
 double getMultipleIntegralUsingRectangleMethod(double (*function)(double, double), double x1, double x2, double y1, double y2, double stepX, double stepY) {
 	double resultIntegral = 0;
 	double cellSquare = stepX * stepY;
-
 
 	for (double i = x1; i < x2; i += stepX) {
 		for (double j = y1; j < y2; j += stepY) {
@@ -25,8 +25,10 @@ double function1(double x, double y) {
 }
 
 int main() {
-	double (*passingFunc)(double, double) = null;
+	double (*passingFunc)(double, double) = nullptr;
 	passingFunc = &function1;
 
 	double integral = getMultipleIntegralUsingRectangleMethod(passingFunc, 0, 2, 0, 2, 0.1, 0.1);
+
+	std::cout << integral;
 }
