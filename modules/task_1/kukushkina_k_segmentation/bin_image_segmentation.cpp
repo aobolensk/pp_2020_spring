87e6cdc3002645a4387e5833b3aedd4b90f8baa8
381706-1Kukushkina <39962007+Alice105973@users.noreply.github.com>
2020-03-19 11:17:33 +0300

    initial commit

---
+++ modules/task_1/kukushkina_k_segmentation/bin_image_segmentation.cpp
@@ -0,0 +1,2 @@
+// Copyright 2020 Kukushkina Ksenia
+#include "../../modules/task_1/kukushkina_k_segmentation/bin_image_segmentation.h"
---
+++ modules/task_1/kukushkina_k_segmentation/main.cpp
@@ -0,0 +1,8 @@
+// Copyright 2020 Kukushkina Ksenia
+#include <gtest/gtest.h>
+#include "./bin_image_segmentation.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
