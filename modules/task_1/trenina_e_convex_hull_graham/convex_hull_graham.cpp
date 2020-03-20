82763743568e9df7ad85a68266a99a937320cd69
LizbethTr <elrtr@yandex.ru>
2020-03-20 19:02:06 +0300

    Init

---
+++ modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Trenina Elizaveta
+
+#include <algorithm>
+#include <utility>
+#include <random>
+#include <vector>
+#include <ctime>
+#include "../../../modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.h"
+
+
+
---
+++ modules/task_1/trenina_e_convex_hull_graham/main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2020 Trenina Elizaveta
+
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
+#include "./convex_hull_graham.h"
+
+
