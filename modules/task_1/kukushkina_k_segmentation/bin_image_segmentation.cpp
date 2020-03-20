13a366ad8503d7f50156671a02fc93156fa2b2ed
381706-1Kukushkina <39962007+Alice105973@users.noreply.github.com>
2020-03-20 11:49:43 +0300

    omg it seems to work

--- modules/task_1/kukushkina_k_segmentation/bin_image_segmentation.cpp
+++ modules/task_1/kukushkina_k_segmentation/bin_image_segmentation.cpp
@@ -1,2 +1,71 @@
 // Copyright 2020 Kukushkina Ksenia
 #include "../../modules/task_1/kukushkina_k_segmentation/bin_image_segmentation.h"
+#include <vector>
+#include <random>
+#include <ctime>
+#include <iostream>
+
+static int offset = 0;
+
+std::vector<int> Generate_pic(size_t w, size_t h) {
+  std::vector<int> pic(h * w);
+  for (size_t i = 0; i < h * w; i++) {
+    srand(static_cast<unsigned>(time(0)) + offset++);
+    pic[i] = rand() % 2;
+  }
+  return pic;
+}
+
+void recolor(std::vector<int>* vec, size_t pix, int color, size_t w) {
+  int old = (*vec)[pix];
+  if (old == 0) throw "Trying to recolor empty space";
+  (*vec)[pix] = color;
+  if (pix % w != 0 && (*vec)[pix - 1] != 0 && (*vec)[pix - 1] != color)
+    recolor(vec, pix - 1, color, w);
+  if ((pix + 1) % w != 0 && (*vec)[pix + 1] != 0 && (*vec)[pix - 1] != color)
+    recolor(vec, pix + 1, color, w);
+  if (pix >= w && (*vec)[pix - w] != 0 && (*vec)[pix - 1] != color)
+    recolor(vec, pix - w, color, w);
+  if (pix <= (*vec).size() - w && (*vec)[pix + w] != 0 && (*vec)[pix - 1] != color)
+    recolor(vec, pix + w, color, w);
+}
+
+std::vector<int> Segmentation(const std::vector<int> &source, size_t w) {
+  std::vector<int> res(source);
+  int color = 1;
+  if (res[0] == 1) res[0] = ++color;
+  for (size_t i = 1; i < w; i++) {  // first line segmentation
+    if (res[i] == 0) continue;  // empty space skipping
+    if (res[i - 1] != 0) res[i] = res[i - 1];
+    else res[i] = ++color;  // new segment starting
+  }
+  for (size_t i = w + 1; i < res.size(); i++) {
+    if (res[i] == 0) continue;  // empty space skipping
+    if (i % w == 0) {  // left edge segmentation
+      if (res[i - w] != 0)
+        res[i] = res[i - w];  // adding to existing segment
+      else
+        res[i] = ++color;  // new segment starting
+    }
+    else if (res[i - 1] == 0 && res[i - w] == 0)
+      res[i] = ++color;  // new segment starting
+    else if (res[i - 1] != 0 && res[i - w] == 0)
+      res[i] = res[i - 1];  // adding to left segment
+    else if (res[i - 1] == 0 && res[i - w] != 0)
+      res[i] = res[i - w];  // adding to upper segment
+    else if (res[i - 1] != 0 && res[i - w] != 0) {
+      res[i] = res[i - 1];  // adding to left segment
+      if (res[i - w] != res[i - 1])
+        recolor(&res, i - w, color, w);  // recoloring upper segment
+    }
+  }
+  return res;
+}
+void Output(const std::vector<int>& source, size_t w) {
+  for (size_t i = 0; i < source.size(); i++) {
+    if (i % w == 0)
+      std::cout << std::endl;
+    std::cout << source[i] << " ";
+  }
+  std::cout << std::endl;
+}
--- modules/task_1/kukushkina_k_segmentation/main.cpp
+++ modules/task_1/kukushkina_k_segmentation/main.cpp
@@ -1,9 +1,102 @@
 // Copyright 2020 Kukushkina Ksenia
+
 #include <gtest/gtest.h>
+#include <vector>
+#include <cmath>
 #include "./bin_image_segmentation.h"
 
-TEST(Segmentation, Init) {
-    ASSERT_NO_THROW(1);
+TEST(Bin_image_segmentation, can_generate_image) {
+  ASSERT_NO_THROW(Generate_pic(100, 100));
+}
+
+TEST(Bin_image_segmentation, image_dim) {
+  std::vector<int> vec = Generate_pic(3, 3);
+  ASSERT_EQ(vec.size(), 9);
+}
+
+TEST(Bin_image_segmentation, empty_space_small) {
+  std::vector<int> vec = Generate_pic(10, 10);
+  std::vector<int> res = Segmentation(vec, 10);
+  bool eq = 1;
+  for (size_t i = 0; i < 100; i++) {
+    if ((vec[i] == 0 || res[i] == 0) && (vec[i] != res[i])) {
+      eq = 0;
+      break;
+    }
+  }
+  ASSERT_EQ(1, eq);
+}
+
+TEST(Bin_image_segmentation, empty_space_large) {
+  std::vector<int> vec = Generate_pic(99, 100);  // 999*1000
+  std::vector<int> res = Segmentation(vec, 99);
+  bool eq = 1;
+  for (size_t i = 0; i < 9900; i++) {
+    if ((vec[i] == 0 || res[i] == 0) && (vec[i] != res[i])) {
+      eq = 0;
+      break;
+    }
+  }
+  ASSERT_EQ(1, eq);
+}
+
+TEST(Bin_image_segmentation, correctness_square) {
+  std::vector<int> vec = Generate_pic(10, 10);
+  std::vector<int> res = Segmentation(vec, 10);
+  bool corr = 1;
+  for (size_t i = 11; i < 89; i++) {
+    if (res[i] == 0) continue;
+    if (i % 10 == 0) {
+      if (res[i] != res[i - 10] && res[i - 10] != 0
+        && res[i] != res[i + 10] && res[i + 10] != 0) {
+        corr = 0;
+        break;
+      }
+      if (res[i] == res[i - 1]) {
+        corr = 0;
+        break;
+      }
+    } else {
+      if (res[i] != res[i - 10] && res[i - 10] != 0
+        && res[i] != res[i + 10] && res[i + 10] != 0
+        && res[i] != res[i - 1] && res[i - 1] != 0
+        && res[i] != res[i + 1] && res[i + 1] != 0) {
+        corr = 0;
+        break;
+      }
+    }
+  }
+  ASSERT_EQ(1, corr);
+}
+
+TEST(Bin_image_segmentation, correctness_rectangle) {
+  std::vector<int> vec = Generate_pic(20, 10);
+  std::vector<int> res = Segmentation(vec, 20);
+  bool corr = 1;
+  for (size_t i = 21; i < 179; i++) {
+    if (res[i] == 0) continue;
+    if (i % 20 == 0) {
+      if (res[i] != res[i - 20] && res[i - 20] != 0
+        && res[i] != res[i + 20] && res[i + 20] != 0) {
+        corr = 0;
+        break;
+      }
+      if (res[i] == res[i - 1]) {
+        corr = 0;
+        break;
+      }
+    }
+    else {
+      if (res[i] != res[i - 20] && res[i - 20] != 0
+        && res[i] != res[i + 20] && res[i + 20] != 0
+        && res[i] != res[i - 1] && res[i - 1] != 0
+        && res[i] != res[i + 1] && res[i + 1] != 0) {
+        corr = 0;
+        break;
+      }
+    }
+  }
+  ASSERT_EQ(1, corr);
 }
 
 int main(int argc, char** argv) {
