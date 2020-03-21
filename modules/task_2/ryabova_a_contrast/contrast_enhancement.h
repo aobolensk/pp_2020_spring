af31c492ce2329097399d722d5f83ef1c5dc9277
Alyona <ryabova.alena99@gmail.com>
2020-03-21 21:01:06 +0300

    fix travis 1.2

--- modules/task_2/ryabova_a_contrast/contrast_enhancement.h
+++ modules/task_2/ryabova_a_contrast/contrast_enhancement.h
@@ -1,7 +1,7 @@
 // Copyright 2020 Ryabova Alyona
 
-#ifndef MODULES_TASK_1_RYABOVA_A_CONTRAST_CONTRAST_ENHANCEMENT_H_
-#define MODULES_TASK_1_RYABOVA_A_CONTRAST_CONTRAST_ENHANCEMENT_H_
+#ifndef MODULES_TASK_2_RYABOVA_A_CONTRAST_CONTRAST_OMP_H_
+#define MODULES_TASK_2_RYABOVA_A_CONTRAST_CONTRAST_OMP_H_
 
 #include <omp.h>
 #include <cstdint>
@@ -14,4 +14,4 @@ using Image = std::vector <int>;
 Image randomImage(int width, int height);
 Image contrastImage(Image initImage, int width, int height);
 
-#endif  // MODULES_TASK_1_RYABOVA_A_CONTRAST_CONTRAST_ENHANCEMENT_H_
+#endif  // MODULES_TASK_2_RYABOVA_A_CONTRAST_CONTRAST_OMP_H_
