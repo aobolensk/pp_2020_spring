bab76bc70127d473a2cea93b493a0732a432df93
amyshkin <alexander.myshkin@sarov-itc.ru>
2020-03-23 01:50:44 +0300

    Some fix with code style

---
+++ modules/task_1/myshkin_a_radix_sort_double/main.cpp
@@ -0,0 +1,96 @@
+// Copyright 2020 Myshkin Andrey
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include "gtest/gtest.h"
+#include "../../../modules/task_1/myshkin_a_radix_sort_double/radix_sort_double.h"
+
+
+TEST(Senquential, Test_Numder_First) {
+    const int length = 100;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -1000.0, 1000.0);
+
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, 0);
+}
+
+TEST(Senquential, Test_Numder_Second) {
+    const int length = 1000;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -1000.0, 1000.0);
+
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, 0);
+}
+
+TEST(Senquential, Test_Numder_Third) {
+    const int length = 1500;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -5000.0, 5000.0);
+
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, 0);
+}
+
+TEST(Senquential, Test_Numder_Fourth) {
+    const int length = 2000;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -2500.0, 2500.0);
+
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, 0);
+}
+
+TEST(Senquential, Test_Numder_Five) {
+    const int length = -100;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -3000.0, 3000.0);
+    if (sts != 0) { ASSERT_EQ(sts, -1); }
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, -1);
+}
+
+TEST(Senquential, Test_Numder_Six) {
+    const int length = -15;
+    int sts = 0;
+    double *buffer = reinterpret_cast<double*>(malloc(sizeof(double) * length));
+    sts = getRandomArray(buffer, length, -1000.0, 1000.0);
+    if (sts != 0) { ASSERT_EQ(sts, -1); }
+
+    sts = RadixSort(buffer, length);
+    sts = SortingCheck(buffer, length);
+    if (buffer) { free(buffer); buffer = NULL; }
+
+    ASSERT_EQ(sts, -1);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
