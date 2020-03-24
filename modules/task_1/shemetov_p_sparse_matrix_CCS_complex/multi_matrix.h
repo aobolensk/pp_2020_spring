b71530699d54636350e83cf2367785818d85b5b4
Philipp Shemetov <filshe1512@gmail.com>
2020-03-24 15:29:36 +0300

    One hundred eighty sixth steps on Titan

--- modules/task_1/shemetov_p_sparse_matrix_CCS_complex/multi_matrix.h
+++ modules/task_1/shemetov_p_sparse_matrix_CCS_complex/multi_matrix.h
@@ -1,30 +1,50 @@
-#ifndef MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CSS_COMPLEX_MULTI_MATRIX_H_
-#define MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CSS_COMPLEX_MULTI_MATRIX_H_
+// Copyright 2020 Shemetov Philipp
+
+#ifndef MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CCS_COMPLEX_MULTI_MATRIX_H_
+#define MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CCS_COMPLEX_MULTI_MATRIX_H_
+
 #include <vector>
 #include <complex>
 
 typedef std::vector<std::vector<std::complex<double>>> mtxComplex;
 
-class SparseMatrixCCS
-{
-private:
-    std::vector<std::complex<int>> value;
+class SparseMatrixCCS {
+ private :
+    std::vector<std::complex<double>> value;
     std::vector<int> row_index;
     std::vector<int> col_offsets;
     size_t n, m;
-    int noneZero;
-public:
-    SparseMatrixCCS(size_t m, size_t n);
-    SparseMatrixCCS(const mtxComplex&);
-    SparseMatrixCCS(const SparseMatrixCCS& obj);
-    void Print(const mtxComplex&);
+ public :
+    SparseMatrixCCS();
+    SparseMatrixCCS(size_t m, size_t n, size_t nonezero);
+
+    SparseMatrixCCS(size_t m, size_t n, double sparseness);
+
+    SparseMatrixCCS(size_t _m, size_t _n,
+                  std::vector<std::complex<double>> _value,
+                  std::vector<int> _row_index,
+                  std::vector<int> _col_offsets);
+
+    explicit SparseMatrixCCS(const mtxComplex &mt);
+
+    SparseMatrixCCS(const SparseMatrixCCS &obj);
+
+    static SparseMatrixCCS MultiplySparseMatrix(const SparseMatrixCCS& A,
+                                                const SparseMatrixCCS& B);
+
+    bool operator==(const SparseMatrixCCS &) const;
+
     void PrintCCS();
-    void TransposePrint(const mtxComplex&);
-    
+
+    SparseMatrixCCS transpose();
 };
 
-mtxComplex randomGenerateMatrix(size_t m,size_t n,double sparseness);
-mtxComplex TransposeMatrix(const mtxComplex&);
 
 
-#endif // MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CSS_COMPLEX_MULTI_MATRIX_H_
+void Print(const mtxComplex &);
+
+mtxComplex multiMatrix(const mtxComplex &mtxA, const mtxComplex &mtxB);
+
+
+
+#endif  //  MODULES_TASK_1_SHEMETOV_P_SPARSE_MATRIX_CCS_COMPLEX_MULTI_MATRIX_H_
