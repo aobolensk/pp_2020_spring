7d14d6958b593818fe615db509aa9d1e73cd5ade
Sokolov Andrey <Sokolov11.nn@gmail.com>
2020-03-30 20:30:27 +0300

    (づ｡◕‿‿◕｡)づ

---
+++ modules/task_2/sokolov_a_sparse_matrix_crs/sparse_matrix_crs_omp.h
@@ -0,0 +1,49 @@
+// Copyright 2020 Sokolov Andrey
+#ifndef MODULES_TASK_2_SOKOLOV_A_SPARSE_MATRIX_CRS_SPARSE_MATRIX_CRS_OMP_H_
+#define MODULES_TASK_2_SOKOLOV_A_SPARSE_MATRIX_CRS_SPARSE_MATRIX_CRS_OMP_H_
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <random>
+#include <utility>
+#include <algorithm>
+#include <vector>
+
+using Matrix = std::vector<std::vector<double>>;
+
+class SparseMatrix {
+ public:
+    size_t rows;
+    size_t cols;
+    std::vector<double> value;
+    std::vector<int> colIndex;  // Column numbers for each item
+    std::vector<int> rowIndex;  // Index of the beginning of each line
+     SparseMatrix() : rows(0), cols(0) {}
+     SparseMatrix(size_t _rows, size_t _cols,
+                  std::vector<double> _value,
+                  std::vector<int>    _colIndex,
+                  std::vector<int>    _rowIndex);
+     explicit SparseMatrix(const Matrix& matrix);
+     SparseMatrix(const SparseMatrix& matrix) = default;
+     SparseMatrix(size_t _rows, size_t _cols, size_t _elemsCount);
+
+     bool operator== (const SparseMatrix& mat) const&;
+
+     Matrix SparseToMatrix();
+
+     void printDefault();
+     void printMatrix();
+
+     friend SparseMatrix SparseMatMul(const SparseMatrix& matrixA, const SparseMatrix& matrixB);
+     friend SparseMatrix SparseMatMulOmp(const SparseMatrix& matrixA, const SparseMatrix& matrixB);
+};
+
+SparseMatrix SparseMatMul(const SparseMatrix& matrixA, const SparseMatrix& matrixB);
+SparseMatrix SparseMatMulOmp(const SparseMatrix& matrixA, const SparseMatrix& matrixB);
+Matrix MatMul(const Matrix& matrixA, const Matrix& matrixB, const double& time);
+
+Matrix generateMatrix(const size_t& rows, const size_t& cols, const size_t& coeff);
+
+void print(const Matrix& matrix);
+
+#endif  // MODULES_TASK_2_SOKOLOV_A_SPARSE_MATRIX_CRS_SPARSE_MATRIX_CRS_OMP_H_
