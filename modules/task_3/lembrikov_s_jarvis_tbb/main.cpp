2130350f3efbbd8ec694a3f3e7d4ae18b91f3ff9
3817061LembrikovStepan <stepan.lembrikov.ru@mail.ru>
2020-04-02 18:35:42 +0300

    everything is OK

--- modules/task_3/lembrikov_s_jarvis_tbb/main.cpp
+++ modules/task_3/lembrikov_s_jarvis_tbb/main.cpp
@@ -53,7 +53,7 @@ TEST(Vector_Min_MPI, Test_On_7_Points_EQ) {
 
     EXPECT_EQ(answer_tbb, answer_seq);
 }
-/*
+
 TEST(Vector_Min_MPI, Test_On_1_Point_EQ) {
     size_t size = 1;
     int num_thr = 2;
@@ -84,40 +84,29 @@ TEST(Vector_Min_MPI, Test_On_2_Equal_Points_EQ) {
     answer_seq = Jarvis_Seq(pr);
 
     EXPECT_EQ(answer_tbb, answer_seq);
-}*/
+}
 
 TEST(Vector_Min_MPI, Test_On_Random_Points_EQ) {
     std::vector<std::pair<double, double>> a;
     std::vector<std::pair<double, double>> answer_seq;
     std::vector<std::pair<double, double>> answer_tbb;
     std::vector<std::pair<double, double>> answer_omp;
-    size_t size_a = 10000000;
+    size_t size_a = 10;
     int num_thr = 2;
-    //double t1, t2, t3;
 
-    //a = getRandomPoints(size_a);
-
-
-    //Proba_While_Tbb(a); 
     a = getRandomPoints(size_a);
-    //t1 = omp_get_wtime();
-    tbb::tick_count t1 = tbb::tick_count::now();
+    // tbb::tick_count t1 = tbb::tick_count::now();
     answer_seq = Jarvis_Seq(a);
-    tbb::tick_count t2 = tbb::tick_count::now();
-    //t2 = omp_get_wtime();
+    // tbb::tick_count t2 = tbb::tick_count::now();
     answer_tbb = Jarvis_Tbb(a, num_thr);
-    tbb::tick_count t3 = tbb::tick_count::now();
-    //answer_omp = Jarvis_Omp(a, num_thr);
-    tbb::tick_count t4 = tbb::tick_count::now();
-    //t3 = omp_get_wtime();
+    // tbb::tick_count t3 = tbb::tick_count::now();
 
-    std::cout << "seq_time = " << (t2 - t1).seconds() << "\n";
-    std::cout << "tbb_time = " << (t3 - t2).seconds() << "\n";
-    std::cout << "omp_time = " << (t4 - t3).seconds() << "\n";
+    // std::cout << "seq_time = " << (t2 - t1).seconds() << "\n";
+    // std::cout << "tbb_time = " << (t3 - t2).seconds() << "\n";
 
     EXPECT_EQ(answer_seq, answer_tbb);
 }
-/*
+
 TEST(Vector_Min_MPI, Test_On_Negative_Points_EQ) {
     std::vector<std::pair<double, double>> a;
     std::vector<std::pair<double, double>> answer_seq;
@@ -130,7 +119,7 @@ TEST(Vector_Min_MPI, Test_On_Negative_Points_EQ) {
     answer_seq = Jarvis_Seq(a);
 
     EXPECT_EQ(answer_seq, answer_tbb);
-}*/
+}
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
