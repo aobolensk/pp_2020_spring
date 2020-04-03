58a0b59251e3672a249e9bff6462bf88602d565d
yasakova-anastasiya <yasakova_anastasiya@mail.ru>
2020-04-03 12:17:41 +0300

    commit 2

--- modules/task_2/yasakova_a_jarvis_alg/jarvis_alg.cpp
+++ modules/task_2/yasakova_a_jarvis_alg/jarvis_alg.cpp
@@ -22,6 +22,7 @@ std::vector < std::pair<int, int>> GetRandomPoints(int n) {
 
 std::vector < std::pair<int, int>> omp_JarvisAlg(const std::vector<std::pair<int, int>>& points) {
     std::vector<std::pair<int, int>> res;
+    int size = points.size();
     res.push_back(omp_FindFirstPoint(points));
     std::pair<int, int> tmp = omp_FindSecondPoint(points, res[0]);
 
@@ -37,7 +38,7 @@ std::vector < std::pair<int, int>> omp_JarvisAlg(const std::vector<std::pair<int
         {
             int tid = omp_get_thread_num();
 #pragma omp for
-            for (int i = 0; i < points.size(); ++i) {
+            for (int i = 0; i < size; ++i) {
                 double curr_cos = CountCos(res[res.size() - 1], res[res.size() - 2], points[i]);
                 if (curr_cos < min_cos_[tid]) {
                     min_cos_[tid] = curr_cos;
@@ -63,11 +64,12 @@ std::vector < std::pair<int, int>> omp_JarvisAlg(const std::vector<std::pair<int
 }
 
 std::pair<int, int> omp_FindFirstPoint(const std::vector<std::pair<int, int>>& points) {
-    std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
+    std::pair<int, int> res = points[0];
+    int size = points.size();
     int nthreads = 4;
 #pragma omp parallel shared(res) num_threads(nthreads)
 #pragma omp for
-    for (int i = 0; i < points.size(); ++i) {
+    for (int i = 0; i < size; ++i) {
 #pragma omp critical
         if (points[i].second < res.second)
             res = points[i];
@@ -78,12 +80,13 @@ std::pair<int, int> omp_FindFirstPoint(const std::vector<std::pair<int, int>>& p
 }
 
 std::pair<int, int> omp_FindSecondPoint(const std::vector<std::pair<int, int>>& points, std::pair<int, int> tmp) {
-    std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
+    std::pair<int, int> res = points[0];
     double min = 4;
     int nthreads = 4;
+    int size = points.size();
 #pragma omp parallel shared(res, min) num_threads(nthreads)
 #pragma omp for
-    for (int i = 0; i < points.size(); ++i) {
+    for (int i = 0; i < size; ++i) {
 #pragma omp critical
         if (points[i] != tmp) {
             double angle = atan(static_cast<double>(points[i].second - tmp.second)
@@ -113,12 +116,13 @@ double distance(std::pair<int, int> a, std::pair<int, int> b) {
 
 std::vector < std::pair<int, int>> seq_JarvisAlg(const std::vector<std::pair<int, int>>& points) {
     std::vector<std::pair<int, int>> res;
+    int size = points.size();
     res.push_back(seq_FindFirstPoint(points));
     std::pair<int, int> tmp = seq_FindSecondPoint(points, res[0]);
     while (tmp != res[0]) {
         res.push_back(tmp);
         double min_cos = 1;
-        for (int i = 0; i < points.size(); ++i) {
+        for (int i = 0; i < size; ++i) {
             double curr_cos = CountCos(res[res.size() - 1], res[res.size() - 2], points[i]);
             if (curr_cos < min_cos) {
                 min_cos = curr_cos;
@@ -133,8 +137,9 @@ std::vector < std::pair<int, int>> seq_JarvisAlg(const std::vector<std::pair<int
 }
 
 std::pair<int, int> seq_FindFirstPoint(const std::vector<std::pair<int, int>>& points) {
-    std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
-    for (int i = 0; i < points.size(); ++i) {
+    std::pair<int, int> res = points[0];
+    int size = points.size();
+    for (int i = 0; i < size; ++i) {
         if (points[i].second < res.second)
             res = points[i];
         else if (points[i].second == res.second && points[i].first < res.first)
@@ -144,9 +149,10 @@ std::pair<int, int> seq_FindFirstPoint(const std::vector<std::pair<int, int>>& p
 }
 
 std::pair<int, int> seq_FindSecondPoint(const std::vector<std::pair<int, int>>& points, std::pair<int, int> tmp) {
-    std::pair<int, int> res = std::make_pair(points[0].first, points[0].second);
+    std::pair<int, int> res = points[0];
     double min = 4;
-    for (int i = 0; i < points.size(); ++i) {
+    int size = points.size();
+    for (int i = 0; i < size; ++i) {
         if (points[i] != tmp) {
             double angle = atan(static_cast<double>(points[i].second - tmp.second)
                 / static_cast<double>(abs(points[i].first - tmp.first)));
