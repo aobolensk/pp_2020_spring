4e2bcb1600a151a63d0e34cff8d2a73994038630
381706-1Mityagina <darya.mityagina.1999@mail.com>
2020-04-04 14:02:30 +0300

    Small changes

--- modules/task_2/mityagina_d_increasing_the_contrast/increasing_the_contrast.cpp
+++ modules/task_2/mityagina_d_increasing_the_contrast/increasing_the_contrast.cpp
@@ -23,9 +23,13 @@ std::vector <int> generate_im(int width_im, int height_im) {
   int size = width_im * height_im;
   gen.seed(static_cast<unsigned int>(time(0)));
   std::vector <int> grayscale_image(size);
+
+// #pragma omp parallel for
+{
   for (int i = 0; i < size; ++i) {
     grayscale_image[i] = gen() % 255;
   }
+}
   return grayscale_image;
 }
 
@@ -61,18 +65,37 @@ std::vector <int> contrast_increase(std::vector<int> grayscale_image, int width,
   std::vector<int> output(grayscale_image);
   int min = minimum(&grayscale_image);
   int max = maximum(&grayscale_image);
-  int step = size / num;
-  int i = 0;
+  int step = 0, current = 0, start = 0, i = 0;
 
-  #pragma omp parallel private(i) shared(output, grayscale_image) num_threads(num)
+  if (max < min)
+    throw -1;
+
+  if (max != 255 && min != 0)
   {
-    omp_set_num_threads(num);
-    int current = omp_get_thread_num();
-    int start = MIN(current * step, size - (current + 1) * step);
-    #pragma omp parallel for
-    for (i = start; i < start + step; i++) {
-      output[i] = use_formula(grayscale_image[i], max, min);
+    // #pragma omp parallel// num_threads(num)
+#pragma omp parallel private(i, current, step, start) shared(output, grayscale_image, min, max) num_threads(num)
+    {
+      omp_set_num_threads(num);
+      current = omp_get_thread_num();
+      start = current * step;
+      step = MIN(size / num, size - current * (size / num));
+
+      #pragma omp for
+      {
+          for (i = start; i < start + step; i++) {
+            float a = (-1) * (static_cast<float>(255) / (max - min)) * min;
+            float b = static_cast<float>(255) / (max - min);
+            if (max == min)
+              output[i] =  0;
+            else
+              output[i] = (a + b * grayscale_image[i]);
+          }
+      }
     }
+    return output;
   }
-  return output;
+  else
+  {
+    return grayscale_image;
+  }
 }
