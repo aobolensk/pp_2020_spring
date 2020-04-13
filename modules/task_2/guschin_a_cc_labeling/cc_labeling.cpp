1b6802093909aa0084672394d3deb200245d7fb2
Alexander Guschin <igushin1999@gmail.com>
2020-04-13 15:47:53 +0300

    added simple merge

--- modules/task_2/guschin_a_cc_labeling/cc_labeling.cpp
+++ modules/task_2/guschin_a_cc_labeling/cc_labeling.cpp
@@ -1,14 +1,8 @@
 // Copyright 2020 Guschin Alexander
-#include "../../../modules/task_1/guschin_a_cc_labeling/cc_labeling.h"
+#include "../../../modules/task_2/guschin_a_cc_labeling/cc_labeling.h"
 #include <omp.h>
 #include <iostream>
 #include <vector>
-
-void Print(const std::vector<std::vector<std::int32_t>>& A);
-void Merge_omp(std::vector<std::vector<std::int32_t>>* ptr,
-               std::int32_t str_row,
-               std::int32_t pr_num, std::int32_t req_num, std::int32_t end_x,
-               std::int32_t end_y);
 
 std::vector<std::vector<std::int32_t>> Labeling_omp(
     const std::vector<std::vector<std::int8_t>>& pic) {
@@ -19,21 +13,19 @@ std::vector<std::vector<std::int32_t>> Labeling_omp(
   std::int32_t h = pic[0].size();
   std::int32_t comp_counter = 1;
   std::int32_t thread_count = 0;
-  //omp_set_num_threads(2);
-#pragma omp parallel
+  omp_set_num_threads(8);
+  #pragma omp parallel
   {
     std::int32_t thread_id = omp_get_thread_num();
     thread_count = omp_get_num_threads();
-#pragma omp critical
+    #pragma omp critical
     std::cout << "thread_count: " << thread_count << std::endl;
 
     std::int32_t first_row;
     if (thread_count >= h)
       first_row = thread_id;
     else
-      first_row = (thread_id == 0
-                       ? 0
-                       : h / thread_count + h % thread_count);
+      first_row = (thread_id == 0 ? 0 : h / thread_count + h % thread_count);
     #pragma omp for
     for (std::int32_t i = 0; i < w; ++i) {
       /*#pragma omp critical
@@ -65,7 +57,7 @@ std::vector<std::vector<std::int32_t>> Labeling_omp(
         } else if (up_value == 0 && left_value == 1) {
           res[i][j] = res[i][j - 1];
         } else if (up_value == 0 && left_value == 0) {
-#pragma omp critical (counter)
+          #pragma omp critical(counter)
           {
             res[i][j] = comp_counter;
             comp_counter++;
@@ -77,36 +69,42 @@ std::vector<std::vector<std::int32_t>> Labeling_omp(
   std::int32_t str_ind = 1;
   std::int32_t shift = 1;
   if (thread_count <= h) {
-    str_ind = h % thread_count;
     shift = h / thread_count;
+    str_ind = h % thread_count + shift;
   }
-    
-  
-  //for (std::int32_t i = str_ind; i < h; i += shift)
-  //  for (std::int32_t j = 0; j < w; ++j)
-  //    if ()
+
+  std::cout << "shift " << shift << std::endl;
+  std::cout << "str ind " << str_ind << std::endl;
+
+  for (std::int32_t i = str_ind; i < h; i += shift)
+    for (std::int32_t j = 0; j < w; ++j) {
+      if (res[i][j] == 0 || res[i - 1][j] == 0 || res[i][j] == res[i - 1][j])
+        continue;
+      else
+        Merge_omp(&res, 0, res[i - 1][j], res[i][j], i, j);
+    }
   Print(res);
   std::cout << "num " << thread_count;
   return res;
 }
 
-void Merge_omp(std::vector<std::vector<std::int32_t>>* ptr,
-               std::int32_t str_row, std::int32_t pr_num, std::int32_t req_num,
-               std::int32_t end_x, std::int32_t end_y) {
-  std::int32_t w = (*ptr).size();
-  std::int32_t h = (*ptr)[0].size();
-  for (std::int32_t i = 0; i < w; ++i)
-    for (std::int32_t j = str_row; j < h; ++j) {
-      if ((*ptr)[i][j] == pr_num) (*ptr)[i][j] = req_num;
-      if (i == end_x && j == end_y) return;
-    }
-}
-
-void Print(const std::vector<std::vector<std::int32_t>>& A) {
-  std::int32_t h = A.size();
-  std::int32_t w = A[0].size();
-  for (std::int32_t i = 0; i < h; ++i) {
-    for (std::int32_t j = 0; j < w; ++j) std::cout << A[i][j] << " ";
-    std::cout << std::endl;
+  void Merge_omp(std::vector<std::vector<std::int32_t>> * ptr,
+                 std::int32_t str_row, std::int32_t pr_num,
+                 std::int32_t req_num, std::int32_t end_x, std::int32_t end_y) {
+    std::int32_t w = (*ptr).size();
+    std::int32_t h = (*ptr)[0].size();
+    for (std::int32_t i = 0; i < w; ++i)
+      for (std::int32_t j = str_row; j < h; ++j) {
+        if ((*ptr)[i][j] == pr_num) (*ptr)[i][j] = req_num;
+        if (i == end_x && j == end_y) return;
+      }
   }
-}
+
+  void Print(const std::vector<std::vector<std::int32_t>>& A) {
+    std::int32_t h = A.size();
+    std::int32_t w = A[0].size();
+    for (std::int32_t i = 0; i < h; ++i) {
+      for (std::int32_t j = 0; j < w; ++j) std::cout << A[i][j] << " ";
+      std::cout << std::endl;
+    }
+  }
--- modules/task_2/guschin_a_cc_labeling/cc_labeling.h
+++ modules/task_2/guschin_a_cc_labeling/cc_labeling.h
@@ -1,16 +1,18 @@
 // Copyright 2020 Guschin Alexander
 #ifndef MODULES_TASK_2_GUSCHIN_A_CC_LABELING_CC_LABELING_H_
 #define MODULES_TASK_2_GUSCHIN_A_CC_LABELING_CC_LABELING_H_
+#include <omp.h>
 #include <string>
 #include <vector>
-#include <omp.h>
 #include <iostream>
 
 
 std::vector<std::vector<int>> Labeling_omp(
     const std::vector<std::vector<std::int8_t>>& pic);
-void Merge_omp(std::vector<std::vector<int>>* ptr, int str_row, int pr_num,
-               int req_num, int end_x, int end_y);
+void Merge_omp(std::vector<std::vector<int>>* ptr, std::int32_t str_row,
+               std::int32_t pr_num, std::int32_t req_num, std::int32_t end_x,
+               std::int32_t end_y);
 void Print(const std::vector<std::vector<std::int32_t>>& A);
+
 
 #endif  // MODULES_TASK_2_GUSCHIN_A_CC_LABELING_CC_LABELING_H_
