20b77750d236783b97d06e6958b8ff578053713b
adudchen <da394372@gmail.com>
2020-04-17 17:26:20 +0300

    add tests

--- modules/task_2/dudchenko_a_sobel_omp/dudchenko_a_sobel_omp.h
+++ modules/task_2/dudchenko_a_sobel_omp/dudchenko_a_sobel_omp.h
@@ -1,7 +1,7 @@
 // Copyright 2020 Dudchenko Anton
 
-#ifndef MODULES_TASK_1_DUDCHENKO_A_SOBEL_DUDCHENKO_A_SOBEL_H_
-#define MODULES_TASK_1_DUDCHENKO_A_SOBEL_DUDCHENKO_A_SOBEL_H_
+#ifndef MODULES_TASK_2_DUDCHENKO_A_SOBEL_OMP_DUDCHENKO_A_SOBEL_OMP_H_
+#define MODULES_TASK_2_DUDCHENKO_A_SOBEL_OMP_DUDCHENKO_A_SOBEL_OMP_H_
 
 #include <vector>
 
@@ -12,7 +12,11 @@ struct image {
     size_t _rows;
     image(std::vector<T> matrix, size_t columns, size_t rows):_matrix(matrix),
                                                               _columns(columns),
-                                                              _rows(rows) {}
+                                                              _rows(rows) {
+        if (matrix.size() != rows * columns)
+            throw -1;
+    }
+
     image(size_t columns, size_t rows):_columns(columns),
                                        _rows(rows) {
         if (rows <= 0 || columns <= 0) {
@@ -33,4 +37,4 @@ image<uint8_t> sobelSequence(image<uint8_t> inImage);
 image<uint8_t> sobelOmp(image<uint8_t> inImage);
 image<uint8_t> randImage(size_t r, size_t c);
 
-#endif  // MODULES_TASK_1_DUDCHENKO_A_SOBEL_DUDCHENKO_A_SOBEL_H_
+#endif  // MODULES_TASK_2_DUDCHENKO_A_SOBEL_OMP_DUDCHENKO_A_SOBEL_OMP_H_
--- modules/task_2/dudchenko_a_sobel_omp/main.cpp
+++ modules/task_2/dudchenko_a_sobel_omp/main.cpp
@@ -5,12 +5,29 @@
 #include "gtest/gtest.h"
 #include "../../../modules/task_2/dudchenko_a_sobel_omp/dudchenko_a_sobel_omp.h"
 
-TEST(seqSobelFilter, canCreateRandomMatrix) {
+TEST(imageStructure, canCompareImages) {
+    image<int> img1({1, 2, 3, 4}, 2, 2);
+    image<int> img2({1, 2, 3, 4}, 2, 2);
+    image<int> img3({1, 0, 3, 4}, 2, 2);
+    image<int> img4({1, 2, 3, 4}, 1, 4);
+
+    ASSERT_TRUE(img1 == img1);
+    ASSERT_TRUE(img1 == img2);
+
+    ASSERT_FALSE(img1 == img3);
+    ASSERT_FALSE(img1 == img4);
+}
+
+TEST(imageStructure, canCreateRandomMatrix) {
     ASSERT_NO_THROW(randImage(5, 5));
 }
 
-TEST(seqSobelFilter, randMatrixWithNullSize) {
+TEST(imageStructure, randMatrixWithNullSize) {
     ASSERT_ANY_THROW(randImage(0, 0));
+}
+
+TEST(imageStructure, createMatrixWithIncorrectSize) {
+    ASSERT_ANY_THROW(image<int> ({1, 2, 3, 4}, 1, 2));
 }
 
 TEST(seqSobelFilter, emptyMatrix) {
@@ -41,43 +58,53 @@ TEST(parallelSobelFilter, sobelChangeMatrix) {
 TEST(parallelSobelFilter, seqAndParIsEqual) {
     auto img = randImage(1000, 1000);
 
-    auto startPar = omp_get_wtime();
+//    auto startPar = omp_get_wtime();
     auto parRes = sobelOmp(img);
-    auto stopPar = omp_get_wtime();
+//    auto stopPar = omp_get_wtime();
 
-    auto startSeq = omp_get_wtime();
+//    auto startSeq = omp_get_wtime();/
     auto seqRes = sobelSequence(img);
-    auto stopSeq = omp_get_wtime();
+//    auto stopSeq = omp_get_wtime();
 
-    std::cout << "Sequnce: " << stopSeq - startSeq << "\n"
-              << "Parallel: " << stopPar - startPar << "\n"
-              << "Delta: " << stopSeq - startSeq - stopPar + startPar << "\n";
+//    std::cout << "Sequnce: " << stopSeq - startSeq << "\n"
+//              << "Parallel: " << stopPar - startPar << "\n"
+//              << "Delta: " << stopSeq - startSeq - stopPar + startPar << "\n";
 
     ASSERT_EQ(parRes, seqRes);
 }
 
 TEST(parallelSobelFilter, returnExpectedResult) {
-    std::vector<uint8_t> inMatrix(100);
-
-    for (uint8_t i = 0; i < 100; i++) {
-        inMatrix[i] = i;
+    std::vector<uint8_t> inMatrix(100, 50);
+    for (uint8_t i = 0; i < 10; i++) {
+        inMatrix[i * 10 + i] = 200;  // Diagonal of light pixels.
     }
 
-    std::vector<uint8_t> expectedMatrix = {};
+    std::vector<uint8_t> expectedMatrix(100, 0);
+
+    for (int i = 2; i < 9; i++) {
+        expectedMatrix[i * 10 + i - 1] = 255;  // White pixels above and below main diagonal.
+        expectedMatrix[(i - 1) * 10 + i] = 255;
+        int color = 212;
+        expectedMatrix[(i + 1) * 10 + i - 1] = color;  // Gray pixels below white pixels.
+        expectedMatrix[(i - 1) * 10 + i + 1] = color;  // Gray pixels above white pixels.
+    }
+
+    for (int i = 0; i < 10; i++) {
+        // Black border (3x3 mask).
+        expectedMatrix[i] = 0;
+        expectedMatrix[i * 10] = 0;
+        expectedMatrix[i * 10 + 9] = 0;
+        expectedMatrix[90 + i] = 0;
+    }
 
     image<uint8_t> inImage(inMatrix, 10, 10);
-//    image<uint_8> expectedImage(expectedMatrix, 10, 10);
+    image<uint8_t> expectedImage(expectedMatrix, 10, 10);
 
-    for (auto el : inImage._matrix) {
-        std::cout << (unsigned int)el << " ";
-    }
-    std::cout << "\n";
+    auto parResult = sobelOmp(inImage);
+    auto seqResult = sobelSequence(inImage);
 
-    auto result = sobelOmp(inImage);
-    for (auto el : result._matrix) {
-        std::cout << (unsigned int)el << " ";
-    }
-//    ASSERT_EQ(result, expectedImage);
+    ASSERT_EQ(seqResult, parResult);
+    ASSERT_EQ(parResult, expectedImage);
 }
 
 int main(int argc, char **argv) {
