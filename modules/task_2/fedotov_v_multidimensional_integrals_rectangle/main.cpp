d75b46c73753d05fff8acfd885d2c8c59aee0425
Vlad Fedotov <fedotovvlad@list.ru>
2020-04-18 13:51:03 +0300

    time estimation added

--- modules/task_2/fedotov_v_multidimensional_integrals_rectangle/main.cpp
+++ modules/task_2/fedotov_v_multidimensional_integrals_rectangle/main.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <omp.h>
 #include "./integrals-rectangle.h"
+#define PRINTF 1
 
 TEST(MultiDimensionalIntegralRectangle, func1) {
     double x1 = 0;
@@ -12,8 +13,12 @@ TEST(MultiDimensionalIntegralRectangle, func1) {
     double stepY = 0.01;
     double (*passingFunc)(double, double) = &function1;
     double trueIntegral = 10.67;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 1 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.1);
 }
@@ -27,8 +32,12 @@ TEST(MultiDimensionalIntegralRectangle, func2) {
     double stepY = 0.01;
     double (*passingFunc)(double, double) = &function2;
     double trueIntegral = 1.59;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 2 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.001);
 }
@@ -42,8 +51,12 @@ TEST(MultiDimensionalIntegralRectangle, func3) {
     double stepY = 0.01;
     double (*passingFunc)(double, double) = &function3;
     double trueIntegral = -0.34;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 3 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.1);
 }
@@ -57,8 +70,12 @@ TEST(MultiDimensionalIntegralRectangle, func4) {
     double stepY = 0.01;
     double (*passingFunc)(double, double) = &function4;
     double trueIntegral = 1.76;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 4 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.1);
 }
@@ -72,8 +89,12 @@ TEST(MultiDimensionalIntegralRectangle, func5) {
     double stepY = 0.001;
     double (*passingFunc)(double, double) = &function5;
     double trueIntegral = 2880;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 5 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.1);
 }
@@ -87,8 +108,12 @@ TEST(MultiDimensionalIntegralRectangle, func6) {
     double stepY = 0.1;
     double (*passingFunc)(double, double) = &function6;
     double trueIntegral = 2.25;
+    double startTime, endTime;
+    startTime = omp_get_wtime();
     double integral = getMultipleIntegralUsingRectangleMethod(passingFunc,
                x1, x2, y1, y2, stepX, stepY);
+    endTime = omp_get_wtime();
+    if (PRINTF) printf("Func 6 test time: %f", endTime - startTime);
 
     ASSERT_NEAR(trueIntegral, integral, 0.1);
 }
