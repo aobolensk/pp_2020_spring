6890c91e16b7a6cd0fc4d1df3b043509351aaf98
Татьяна Кондрина <kondrina.tanya@gmail.com>
2020-04-18 14:06:40 +0300

    all done

---
+++ modules/task_2/kondrina_t_jarvis_omp/jarvis_omp.h
@@ -0,0 +1,29 @@
+// Copyright 2020 Kondrina Tatyana
+
+#ifndef MODULES_TASK_2_KONDRINA_T_JARVIS_OMP_JARVIS_H_
+#define MODULES_TASK_2_KONDRINA_T_JARVIS_OMP_JARVIS_H_
+
+#include <vector>
+#include <omp.h>
+
+struct Point {
+  double x = 0;
+  double y = 0;
+
+  friend bool operator!=(const Point& a, const Point& b) {
+    return !(a == b);
+  }
+  friend bool operator==(const Point& a, const Point& b) {
+    return (a.x == b.x) && (a.y == b.y);
+  }
+
+  friend bool operator<(const Point& a, const Point& b) {
+    return (a.y < b.y) || (a.y == b.y && a.x < b.x);
+  }
+};
+
+std::vector<Point> randomSet(int point_count);
+std::vector<Point> buildHull_seq(std::vector<Point> m_set);
+std::vector<Point> buildHull_omp(std::vector<Point> m_set);
+
+#endif  // MODULES_TASK_2_KONDRINA_T_JARVIS_OPM_JARVIS_H_
