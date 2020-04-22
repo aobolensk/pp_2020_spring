fe4abef57940b7f2dd8593ebb9c7f1dd58c4a61c
megarungle <megarungle@gmail.com>
2020-04-23 01:01:46 +0300

    Implement Intel TBB version

--- modules/task_3/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
+++ modules/task_3/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
@@ -4,4 +4,9 @@
 
 #include <vector>
 
+std::vector<std::vector<int>> getRandomPic(int n, int m);
+
+std::vector<std::vector<int>> addBorders(std::vector<std::vector<int>> pic, int n, int m);
+std::vector<std::vector<int>> gaussFilter(std::vector<std::vector<int>> pic, int n, int m);
+
 #endif  // MODULES_TASK_3_UTKIN_K_LIN_IMG_FILTER_GAUSS_VERT_LIN_IMG_FILTER_GAUSS_VERT_H_
