86aa3beb8d0dd034621871629afed84fe1c2dfff
megarungle <megarungle@gmail.com>
2020-04-22 23:37:10 +0300

    Initial commit

---
+++ modules/task_3/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.cpp
@@ -0,0 +1,5 @@
+// Copyright 2020 Utkin Konstantin
+#include <vector>
+#include <random>
+#include <ctime>
+#include "../../../modules/task_3/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h"
---
+++ modules/task_3/utkin_k_lin_img_filter_gauss_vert/main.cpp
@@ -0,0 +1,4 @@
+// Copyright 2020 Utkin Konstantin
+#include <gtest/gtest.h>
+#include <vector>
+#include "./lin_img_filter_gauss_vert.h"
