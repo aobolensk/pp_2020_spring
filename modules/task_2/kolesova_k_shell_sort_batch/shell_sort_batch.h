3492e9a09142c1c8f33809ebc005bde7dc38c6b2
Кристина <miss-yurievna@ya.ru>
2020-04-25 20:33:43 +0300

    test2-1

--- modules/task_2/kolesova_k_shell_sort_batch/shell_sort_batch.h
+++ modules/task_2/kolesova_k_shell_sort_batch/shell_sort_batch.h
@@ -11,4 +11,4 @@ std::vector<int> shuffle(const std::vector<int>& vec, int l, int r);
 std::vector<int> unshuffle(const std::vector<int>& vec, int l, int r);
 std::vector<int> shellBetchSort_omp(const std::vector<int>& vec);
 
-#endif //  MODULES_TASK_2_KOLESOVA_K_SHELL_SORT_BATCH_SHELL_SORT_BATCH_H_
+#endif  // MODULES_TASK_2_KOLESOVA_K_SHELL_SORT_BATCH_SHELL_SORT_BATCH_H_
