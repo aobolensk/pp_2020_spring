bd79b596b78a9e7b03c7f95394bdf8503e4a5120
megarungle <megarungle@gmail.com>
2020-04-26 02:35:31 +0300

    Implement std::thread version

--- modules/task_4/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
+++ modules/task_4/utkin_k_lin_img_filter_gauss_vert/lin_img_filter_gauss_vert.h
@@ -4,4 +4,16 @@
 
 #include <vector>
 
+constexpr auto NUM_THREADS = 6;
+
+std::vector<std::vector<int>> getRandomPic(int n, int m);
+
+void threadFunction(std::vector<std::vector<int>>& in, std::vector<std::vector<int>>* out,
+                    int n, int mStart, int m);
+
+void tf(std::vector<std::vector<int>>* out);
+
+std::vector<std::vector<int>> addBorders(std::vector<std::vector<int>> pic, int n, int m);
+std::vector<std::vector<int>> gaussFilter(std::vector<std::vector<int>> pic, int n, int m);
+
 #endif  // MODULES_TASK_4_UTKIN_K_LIN_IMG_FILTER_GAUSS_VERT_LIN_IMG_FILTER_GAUSS_VERT_H_
