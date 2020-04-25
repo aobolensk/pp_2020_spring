60ab7b685bf83c49eb3083cb0df27834eb05e28f
Кристина <miss-yurievna@ya.ru>
2020-04-25 19:34:55 +0300

    test2-1

--- modules/task_1/kolesova_k_shell_sort_batch/shell_sort_batch.h
+++
@@ -1,6 +0,0 @@
-
-#include <vector>
-
-std::vector<int> randVec(int size);
-std::vector<int> sortShell(std::vector<int>& vec, int l, int r);
-std::vector<int> oneShellBetchSort(std::vector<int>& vec);
