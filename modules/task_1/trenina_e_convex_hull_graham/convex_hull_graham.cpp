bf780c129f90567136d108247df8cf05eabbc528
LizbethTr <elrtr@yandex.ru>
2020-05-15 17:52:07 +0300

    seq_fixtabs

--- modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.cpp
+++ modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.cpp
@@ -1,87 +1,76 @@
 // Copyright 2020 Trenina Elizaveta
 
 #include <algorithm>
-#include <utility>
 #include <random>
 #include <vector>
 #include <ctime>
 #include "../../../modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.h"
 
 bool rotate(const point& A, const point& B, const point& C) {// left?
-	return (B.x - A.x) * (C.y - B.y) - (B.y - A.y) * (C.x - B.x) >= 0;
+    return (B.x - A.x) * (C.y - B.y) - (B.y - A.y) * (C.x - B.x) >= 0;
 }
 
-bool cmp(point B, point C) {
-//bool cmp(const point& B, const point& C) {// B<C?
-	return B.x * (C.y - B.y) - B.y * (C.x - B.y) < 0 ||
-		B.x * (C.y - B.y) - B.y * (C.x - B.y) == 0 &&
-		B.x * B.x + B.y * B.y < C.x* C.x + C.y * C.y;
-
+bool cmp(const point& B, const point& C) {// B<C?
+    return
+        C.x * (B.y - C.y) - C.y * (B.x - C.x) < 0 ||
+        C.x * (B.y - C.y) - C.y * (B.x - C.x) == 0 &&
+        B.x * B.x + B.y * B.y < C.x* C.x + C.y * C.y;
 }
 
 
 std::vector<point> Graham(std::vector<point> Q) {
-	if (Q.size() < 3) throw "|Q|<3";
+    if (Q.size() < 3) throw "|Q|<3";
 
-	point p0 = Q[0];
-	for (int i = 1; i < Q.size(); i++) {
-		if ((Q[i].y < p0.y) || (Q[i].y == p0.y) && (Q[i].x < p0.x))
-			p0 = Q[i];
-	}
-	//p0
+    int index = 0;
+    for (int i = 1; i < Q.size(); i++) {
+        if ((Q[i].y < Q[index].y) || (Q[i].y == Q[index].y) && (Q[i].x < Q[index].x))
+            index = i;
+    }
+    std::swap(Q[0], Q[index]);
+    point p0 = Q[0];
 
-	for (auto i : Q) {
-		i.x -= p0.x;
-		i.y -= p0.y;
-	}
-	//p0.x = 0.0;
-	//p0.y = 0.0;
-	//std::sort(Q.begin()+1, Q.end(), &cmp);
+    for (int i = 0; i < Q.size(); i++) {
+        Q[i].x -= p0.x;
+        Q[i].y -= p0.y;
+    }
+    std::sort(Q.begin()+1, Q.end(), &cmp);
 
-	std::vector<point> hull;
-	hull.push_back(Q[0]);
-	hull.push_back(Q[1]);
+    std::vector<point> hull;
+    hull.push_back(Q[0]);
+    hull.push_back(Q[1]);
 
-	size_t i = 2, k = 0;
-	do {
-		point t = Q[i++];
+    size_t i = 2, k = 0;
+    do {
+        point t = Q[i];
 
-		while (!rotate(hull[k], hull[k + 1], t)) {
-			hull.pop_back();
-			--k;
-		}
-		hull.push_back(t);
-		++k;
-	} while (i < Q.size());
+        while (!rotate(hull[k], hull[k + 1], t)) {
+            hull.pop_back();
+            --k;
+        }
+        hull.push_back(t);
+        ++k; ++i;
+    } while (i < Q.size());
 
-	return hull;
+    for (int k = 0; k < hull.size(); k++) {
+        hull[k].x += p0.x;
+        hull[k].y += p0.y;
+    }
+
+    return hull;
 }
 
 
 std::vector<point> GetRandSet(int n) {
-	if (n < 3) throw;
-	std::vector<point> result(n);
+    if (n < 3) throw;
+    std::vector<point> result(n);
+    std::mt19937 gen(time(0));
+    std::uniform_real_distribution<> uid(-100, 100);
 
-	//std::srand(std::time(nullptr));
-
-	//std::generate(result.begin(), result.end(), []() {
-	//	return point{ static_cast<double>(std::rand() % 100 - 50),
-	//				 static_cast<double>(std::rand() % 100 - 50) };
-	//	});
-	//return result;
-
-
-
-	std::mt19937 gen(time(0));
-	//std::uniform_double_distribution<> uid(0, 100);
-	std::uniform_real_distribution<> uid(-100, 100);
-
-	for (int i = 0; i < n; i++) {
-		result[i].x = uid(gen);
-		result[i].y = uid(gen);
-	}
-
-	return result;
+    for (int i = 0; i < n; i++) {
+        result[i].x = uid(gen);
+        result[i].y = uid(gen);
+    }
+    return result;
 }
 
 
--- modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.h
+++ modules/task_1/trenina_e_convex_hull_graham/convex_hull_graham.h
@@ -2,8 +2,6 @@
 #ifndef MODULES_TASK_1_TRENINA_E_CONVEX_HULL_GRAHAM_H_
 #define MODULES_TASK_1_TRENINA_E_CONVEX_HULL_GRAHAM_H_
 
-#include <vector>
-
 struct point
 {
     double x, y;
@@ -13,18 +11,18 @@ struct point
     }
 
     point(double _x, double _y) : x(_x), y(_y) {}
+
+    friend bool operator==(const point& a, const point& b) {
+        return (a.x == b.x) && (a.y == b.y);
+    }
 };
 
+bool cmp(const point& B, const point& C); // B<C?
 
-bool cmp(point B, point C); // B<C?
+bool rotate(const point& A, const point& B, const point& C);
 
-//std::vector <point> Q;
- bool rotate(const point& A, const point& B, const point& C);
 std::vector<point> GetRandSet(int n);
 
 std::vector<point> Graham(std::vector<point> Q);
-
-
-
 
 #endif  // MODULES_TASK_1_TRENINA_E_CONVEX_HULL_GRAHAM_H_
