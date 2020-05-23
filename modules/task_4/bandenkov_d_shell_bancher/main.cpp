e3c3ac7b781804a0218b06dd739f117973bea862
381706-2BandenkovD <daniilbandenkov@gmail.com>
2020-05-23 03:16:45 +0300

    yspet do o4noi sda4i

--- modules/task_4/bandenkov_d_shell_bancher/main.cpp
+++ modules/task_4/bandenkov_d_shell_bancher/main.cpp
@@ -15,28 +15,28 @@ TEST(test_Shell_Betch_sort_seq, test_on_1000_element) {
   ASSERT_EQ(betch, res);
 }
 
-TEST(test_Shell_Betch_sort_std, test_on_1000_elements_2_thread) {
+TEST(test_Shell_Betch_sort_std, test_on_1000_elements_2_threads) {
   std::vector<int> res = getRandomVector(1000);
   std::vector<int> betch = shellBatcher_std(res, 2, 1000);
   std::sort(res.begin(), res.end());
   ASSERT_EQ(betch, res);
 }
 
-TEST(test_Shell_Betch_sort_std, test_on_1000_elements_4_thread) {
+TEST(test_Shell_Betch_sort_std, test_on_1000_elements_4_threads) {
   std::vector<int> res = getRandomVector(1000);
   std::vector<int> betch = shellBatcher_std(res, 4, 1000);
   std::sort(res.begin(), res.end());
   ASSERT_EQ(betch, res);
 }
 
-TEST(test_Shell_Betch_sort_std, test_on_5000_elements_4_thread) {
+TEST(test_Shell_Betch_sort_std, test_on_5000_elements_4_threads) {
   std::vector<int> res = getRandomVector(5000);
   std::vector<int> betch = shellBatcher_std(res, 4, 5000);
   std::sort(res.begin(), res.end());
   ASSERT_EQ(betch, res);
 }
 
-TEST(test_Shell_Betch_sort_std, test_on_5000_elements_6_thread) {
+TEST(test_Shell_Betch_sort_std, test_on_5000_elements_6_threads) {
   std::vector<int> res = getRandomVector(5000);
   std::vector<int> betch = shellBatcher_std(res, 6, 5000);
   std::sort(res.begin(), res.end());
