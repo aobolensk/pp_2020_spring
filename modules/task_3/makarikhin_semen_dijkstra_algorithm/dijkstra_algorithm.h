d06ed7db9ab3661c30348d60576d6232b02c0e76
Semen Makarikhin <sam20132014@mail.ru>
2020-06-04 16:57:51 +0300

    corr2

--- modules/task_3/makarikhin_semen_dijkstra_algorithm/dijkstra_algorithm.h
+++ modules/task_3/makarikhin_semen_dijkstra_algorithm/dijkstra_algorithm.h
@@ -2,7 +2,6 @@
 #ifndef MODULES_TASK_3_MAKARIKHIN_SEMEN_DIJKSTRA_ALGORITHM_DIJKSTRA_ALGORITHM_H_
 #define MODULES_TASK_3_MAKARIKHIN_SEMEN_DIJKSTRA_ALGORITHM_DIJKSTRA_ALGORITHM_H_
 #include <vector>
-#include "tbb/task.h"
 
 struct Graph {
   std::vector <std::vector<int>> weight_list;
@@ -10,20 +9,8 @@ struct Graph {
 
   explicit Graph(int vertex_n);
 };
-class RooTaskDujkstra : public tbb::task {
- public:
-  RooTaskDujkstra(std::vector<int>* _distance, const Graph& _g,
-    std::vector <bool>* _visit) : distance(_distance),
-    visit(_visit), g(_g) {
-  }
-  tbb::task* execute();
 
- private:
-  std::vector<int>* distance;
-  std::vector <bool>* visit;
-  const Graph& g;
-};
 Graph get_Random_Graph(const int& vertex_n, const int& edge_n);
-std::vector<int> Dijkstra(const Graph& g, int selected_vertex);
+std::vector<int> Dijkstra_Tbb(const Graph& g, int selected_vertex);
 
 #endif  // MODULES_TASK_3_MAKARIKHIN_SEMEN_DIJKSTRA_ALGORITHM_DIJKSTRA_ALGORITHM_H_
